6e374020eddd35465c05a481cbb9fb20c7e38df1
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-08-20 18:23:56 +0100

    DIS-102: feat: Cloudlibray record usage graphs
    fetch and render the relevant usage data

--- code/web/services/CloudLibrary/UsageGraphs.php
+++ code/web/services/CloudLibrary/UsageGraphs.php
@@ -30,6 +30,67 @@ class CloudLibrary_UsageGraphs extends Admin_AbstractUsageGraphs {
 		]);
 	}
 
+	protected function getAndSetInterfaceDataSeries($stat, $instanceName): void {
+		global $interface;
+
+		$dataSeries = [];
+		$columnLabels = [];
+
+		// Load stats from cloud_library_record_usage
+		if ($stat == 'recordsWithUsage' ||
+			$stat == 'loans' ||
+			$stat == 'holds') {
+			$stats = new CloudLibraryRecordUsage();
+			$stats->groupBy('year, month');
+			if (!empty($instanceName)) {
+				$stats->instance = $instanceName;
+			}
+			$stats->selectAdd();
+			$stats->selectAdd('year');
+			$stats->selectAdd('month');
+			$stats->orderBy('year, month');
+
+			if ($stat == 'Records With Usage'){
+				$dataSeries['Records With Usage'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+				$stats->selectAdd('COUNT(id) as recordsUsed');
+			}
+			if ($stat == 'totalHolds'){
+				$dataSeries['Total Holds'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+				$stats->selectAdd('SUM(timesHeld) as totalHolds');
+			}
+			if ($stat == 'totalCheckouts'){
+				$dataSeries['Total Loans'] =  GraphingUtils::getDataSeriesArray(count($dataSeries));
+				$stats->selectAdd('SUM(timesCheckedOut) as totalCheckouts');
+			}
+
+			$stats->find();
+			while ($stats->fetch()) {
+				$curPeriod = "{$stats->month}-{$stats->year}";
+				if (!in_array("{$stats->month}-{$stats->year}", $columnLabels)) {  // prevents the multiple addition of a curPeriod
+					$columnLabels[] = $curPeriod;
+				}
+				if ($stat == 'recordsWithUsage'){
+					/** @noinspection PhpUndefinedFieldInspection */
+					$dataSeries['Records With Usage']['data'][$curPeriod] = $stats->recordsUsed;
+				}
+				if ($stat == 'holds'){
+					/** @noinspection PhpUndefinedFieldInspection */
+					$dataSeries['Total Holds']['data'][$curPeriod] = $stats->totalHolds;
+				}
+				if ($stat == 'loans'){
+					/** @noinspection PhpUndefinedFieldInspection */
+					$dataSeries['Total Loans']['data'][$curPeriod] = $stats->totalCheckouts;
+				}
+
+			}
+		}
+
+		$interface->assign('columnLabels', $columnLabels);
+		$interface->assign('dataSeries', $dataSeries);
+		$interface->assign('translateDataSeries', true);
+		$interface->assign('translateColumnLabels', false);
+	}
+
 	private function assignGraphSpecificTitle($stat) {
 		global $interface;
 		$title = $interface->getVariable('graphTitle');
