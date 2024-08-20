acc7c9595596735ee670537fb1fd3506f9530133
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-08-20 12:30:45 +0100

    DIS-102: refactor: use GraphingUtils

--- code/web/services/OverDrive/UsageGraphs.php
+++ code/web/services/OverDrive/UsageGraphs.php
@@ -4,6 +4,7 @@ require_once ROOT_DIR . '/services/Admin/AbstractUsageGraphs.php';
 require_once ROOT_DIR . '/sys/OverDrive/UserOverDriveUsage.php';
 require_once ROOT_DIR . '/sys/OverDrive/OverDriveRecordUsage.php';
 require_once ROOT_DIR . '/sys/OverDrive/OverDriveStats.php';
+require_once ROOT_DIR . '/sys/Utils/GraphingUtils.php';
 
 
 class OverDrive_UsageGraphs extends Admin_Admin {
@@ -24,91 +25,23 @@ class OverDrive_UsageGraphs extends Admin_Admin {
 		$dataSeries = [];
 		$columnLabels = [];
 
-		$dataSeries['Total Usage'] = [
-			'borderColor' => 'rgba(255, 99, 132, 1)',
-			'backgroundColor' => 'rgba(255, 99, 132, 0.2)',
-			'data' => [],
-		];
-		$dataSeries['Unique Users'] = [
-			'borderColor' => 'rgba(54, 162, 235, 1)',
-			'backgroundColor' => 'rgba(54, 162, 235, 0.2)',
-			'data' => [],
-		];
-		$dataSeries['Records Used'] = [
-			'borderColor' => 'rgba(255, 159, 64, 1)',
-			'backgroundColor' => 'rgba(255, 159, 64, 0.2)',
-			'data' => [],
-		];
-		$dataSeries['Total Holds'] = [
-			'borderColor' => 'rgba(0, 255, 55, 1)',
-			'backgroundColor' => 'rgba(0, 255, 55, 0.2)',
-			'data' => [],
-		];
-		$dataSeries['Total Failed Holds'] = [
-			'borderColor' => 'rgba(255, 55, 0, 1)',
-			'backgroundColor' => 'rgba(255, 55, 0, 0.4)',
-			'data' => [],
-		];
-		$dataSeries['Total Checkouts'] = [
-			'borderColor' => 'rgba(154, 75, 244, 1)',
-			'backgroundColor' => 'rgba(154, 75, 244, 0.2)',
-			'data' => [],
-		];
-		$dataSeries['Total Failed Checkouts'] = [
-			'borderColor' => 'rgba(75, 154, 244, 1)',
-			'backgroundColor' => 'rgba(75, 154, 244, 0.4)',
-			'data' => [],
-		];
-		$dataSeries['Total Renewals'] = [
-			'borderColor' => 'rgba(255, 206, 86, 1)',
-			'backgroundColor' => 'rgba(255, 206, 86, 0.2)',
-			'data' => [],
-		];
-		$dataSeries['Total Early Returns'] = [
-			'borderColor' => 'rgba(75, 192, 192, 1)',
-			'backgroundColor' => 'rgba(75, 192, 192, 0.2)',
-			'data' => [],
-		];
-		$dataSeries['Total Holds Cancelled'] = [
-			'borderColor' => 'rgba(153, 102, 255, 1)',
-			'backgroundColor' => 'rgba(153, 102, 255, 0.2)',
-			'data' => [],
-		];
-		$dataSeries['Total Holds Frozen'] = [
-			'borderColor' => 'rgba(165, 42, 42, 1)',
-			'backgroundColor' => 'rgba(165, 42, 42, 0.2)',
-			'data' => [],
-		];
-		$dataSeries['Total Holds Thawed'] = [
-			'borderColor' => 'rgba(50, 205, 50, 1)',
-			'backgroundColor' => 'rgba(50, 205, 50, 0.2)',
-			'data' => [],
-		];
-		$dataSeries['Total Downloads'] = [
-			'borderColor' => 'rgba(205, 205, 50, 1)',
-			'backgroundColor' => 'rgba(205, 205, 50, 0.4)',
-			'data' => [],
-		];
-		$dataSeries['Total Previews'] = [
-			'borderColor' => 'rgba(50, 205, 205, 1)',
-			'backgroundColor' => 'rgba(50, 205, 205, 0.4)',
-			'data' => [],
-		];
-		$dataSeries['Total Options Updates'] = [
-			'borderColor' => 'rgba(220, 20, 60, 1)',
-			'backgroundColor' => 'rgba(220, 20, 60, 0.4)',
-			'data' => [],
-		];
-		$dataSeries['Total API Errors'] = [
-			'borderColor' => 'rgba(220, 60, 20, 1)',
-			'backgroundColor' => 'rgba(220, 60, 20, 0.2)',
-			'data' => [],
-		];
-		$dataSeries['Total Connection Failures'] = [
-			'borderColor' => 'rgba(255, 165, 0, 1)',
-			'backgroundColor' => 'rgba(255, 165, 0, 0.2)',
-			'data' => [],
-		];
+		$dataSeries['Total Usage'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Unique Users'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Records Used'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Holds'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Failed Holds'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Checkouts'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Failed Checkouts'] = GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Renewals'] =  GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Early Returns'] =  GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Holds Cancelled'] =  GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Holds Frozen'] =  GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Holds Thawed'] =  GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Downloads'] =  GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Previews'] =  GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Options Updates'] =  GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total API Errors'] =  GraphingUtils::getDataSeriesArray(count($dataSeries));
+		$dataSeries['Total Connection Failures'] =  GraphingUtils::getDataSeriesArray(count($dataSeries));
 		$userUsage = new UserOverDriveUsage();
 		$userUsage->groupBy('year, month');
 		if (!empty($instanceName)) {
