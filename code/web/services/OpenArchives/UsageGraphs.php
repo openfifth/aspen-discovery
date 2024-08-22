9d4097ae847c51e816184d533a5d36828b9549c9
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-08-22 17:55:37 +0100

    DIS-102: feat: launch Open Archives usage graphs

---
+++ code/web/services/OpenArchives/UsageGraphs.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once ROOT_DIR . '/services/Admin/AbstractUsageGraphs.php';
+require_once ROOT_DIR . '/sys/OpenArchives/OpenArchivesCollection.php';
+require_once ROOT_DIR . '/sys/OpenArchives/OpenArchivesRecord.php';
+require_once ROOT_DIR . '/sys/OpenArchives/UserOpenArchivesUsage.php';
+require_once ROOT_DIR . '/sys/OpenArchives/OpenArchivesRecordUsage.php';
+require_once ROOT_DIR . '/sys/Utils/GraphingUtils.php';
+
+class OpenArchives_UsageGraphs extends Admin_AbstractUsageGraphs {
+	function launch(): void {
+		$this->launchGraph('OpenArchives');
+	}
+
+	function getBreadcrumbs(): array {
+		$breadcrumbs = [];
+		$breadcrumbs[] = new Breadcrumb('/Admin/Home', 'Administration Home');
+		$breadcrumbs[] = new Breadcrumb('/Admin/Home#side_loads', 'Side Loads');
+		$breadcrumbs[] = new Breadcrumb('/openArchivesCollections/UsageDashboard', 'Usage Dashboard');
+		$breadcrumbs[] = new Breadcrumb('', 'Usage Graph');
+		return $breadcrumbs;
+	}
+
+	function getActiveAdminSection(): string {
+		return 'open_archives';
+	}
+
+	function canView(): bool {
+		return UserAccount::userHasPermission([
+			'View Dashboards',
+			'View System Reports',
+		]);
+	}
+}
