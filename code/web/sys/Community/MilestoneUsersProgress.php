8d129e4463fe98232729a64b3a7df2090cc04d18
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-08-30 11:45:23 +0100

    feat: Get Progress By Milestone Id Method

--- code/web/sys/Community/MilestoneUsersProgress.php
+++ code/web/sys/Community/MilestoneUsersProgress.php
@@ -6,4 +6,14 @@ class MilestoneUsersProgress extends DataObject
     public $userId;
     public $ce_milestone_id;
     public $progress;
+
+    public static function getProgressByMilestoneId($milestoneId, $userId) {
+        $milestoneProgress = new Self();
+
+        $milestoneProgress->whereAdd('ce_milestone_id = ' . intval($milestoneId));
+        $milestoneProgress->whereAdd('userId = ' . intval($userId));
+        $milestoneProgress->find(true);
+
+        return $milestoneProgress->progress ? $milestoneProgress->progress : 0;
+    }
 }
