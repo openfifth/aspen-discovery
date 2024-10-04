94ba39d90a613a1d1967ed49e9ace3b186303900
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-10-04 13:39:52 +0100

    feat: Update correct db file
    Update db file

--- code/web/sys/DBMaintenance/version_updates/24.11.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.11.00.php
@@ -48,6 +48,13 @@ function getUpdates24_11_00(): array {
 		//kodi - ByWater
 
 		//alexander - PTFS-Europe
+		'add_regular_expression_for_iTypes_to_treat_as_eContent' => [
+			'title' => 'Add Regular Expression For iTypes To Treat As eContent',
+			'description' => 'Add treatItemsAsEcontent to give control over iTypes to be treated as eContent',
+			'sql' => [
+				"ALTER TABLE indexing_profiles ADD COLUMN treatItemsAsEcontent VARCHAR(512) DEFAULT 'ebook|ebk|eaudio|evideo|online|oneclick|eaudiobook|download'",
+			],
+		], //add_treatItemsAsEcontent_field
 
 		//chloe - PTFS-Europe
 		'ebsco_passwords_are_stored_as_hash' => [
