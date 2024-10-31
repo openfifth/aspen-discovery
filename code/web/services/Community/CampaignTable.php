728edf8fa94897c377e4d9e723d5f5a2d0d4086b
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-10-31 17:16:41 +0000

    feat: Display Campaign in Campaign Table Page
    Display campaign information in new page when a campaign title is clicked in the main dashboard page.
    
    refactor: Remove var_dump

--- code/web/services/Community/CampaignTable.php
+++ code/web/services/Community/CampaignTable.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once ROOT_DIR . '/sys/Community/Campaign.php';
+require_once ROOT_DIR . '/sys/Community/UserCampaign.php';
+
 require_once ROOT_DIR . '/sys/Community/CampaignMilestone.php';
 
 
@@ -13,12 +15,52 @@ class Community_CampaignTable extends Action {
 
         if ($campaignId > 0) {
             $campaign = Campaign::getCampaignById($campaignId);
+
             if ($campaign) {
                 $interface->assign('campaign', $campaign);
-                $milestones = CampaignMilestone::getMilestoneByCampaign($campaignId);
-                $interface->assign('milestones', $milestones);
                 
+                //Retrieve milestones for the campaign
+                $milestones = CampaignMilestone::getMilestoneByCampaign($campaignId);
 
+                //Get users for campaign
+                $users = $campaign->getUsersForCampaign();
+
+                $userCampaigns = [];
+                foreach ($users as $user) {
+                    $userCampaign = new UserCampaign();
+                    $userCampaign->campaignId = $campaignId;
+                    $userCampaign->userId = $user->id;
+
+                    if ($userCampaign->find(true)) {
+                        $isCampaignComplete = $userCampaign->checkCompletionStatus();
+                        $userCampaigns[$campaign->id][$user->id] = [
+                            'rewardGiven' => (int)$userCampaign->rewardGiven,
+                            'isCampaignComplete' =>$isCampaignComplete,
+                            'milestones' => []
+                        ];
+
+                        //Get milestone completion status
+                        $milestoneCompletionStatus = $userCampaign->checkMilestoneCompletionStatus();
+
+                        foreach ($milestones as $milestone) {
+                            $milestoneComplete = $milestoneCompletionStatus[$milestone->id] ?? false;
+                            $userProgress = MilestoneUsersProgress::getProgressByMilestoneId($milestone->id, $user->id);
+                            $totalGoals = CampaignMilestone::getMilestoneGoalCountByCampaign($campaignId, $milestone->id);
+                            $milestoneRewardGiven = MilestoneUsersProgress::getRewardGivenForMilestone($milestone->id, $user->id);
+
+                            //Add milestone data for each user
+                            $userCampaigns[$campaign->id][$user->id]['milestones'][$milestone->id] = [
+                                'milestoneComplete' => $milestoneComplete,
+                                'userProgress' => $userProgress,
+                                'goal' => $totalGoals,
+                                'milestoneRewardGiven' =>$milestoneRewardGiven,
+                            ];
+                        }
+                    }
+                }
+                $interface->assign('userCampaigns', $userCampaigns);
+                $interface->assign('milestones', $milestones);
+                $interface->assign('users', $users);
 
             } else {
                 $interface->assign('error', 'Campaign not found.');
