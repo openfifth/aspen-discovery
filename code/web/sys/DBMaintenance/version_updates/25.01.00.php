ee6c627538061d32fe64612993a87110a875fac9
Jacob O'Mara <jacob.omara@ptfs-europe.com>
2025-01-29 09:43:34 +0000

    feat: add the ability to disable ils user creation when using sso
    This patch adds the ability to disable the creation of users when using
    SSO for login

--- code/web/sys/DBMaintenance/version_updates/25.01.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.01.00.php
@@ -159,6 +159,18 @@ function getUpdates25_01_00(): array {
 		],	
 		//set_hoopla_index_by_day_to_default
 
+		//jacob - PTFS-Europe
+		'sso_do_not_create_user_in_ils' => [
+			'title' => 'Do not create SSO user in ils',
+			'description' => 'Ability to stop SSO from creating users in the ils',
+			'continueOnError' => true,
+			'sql' => [
+				'ALTER TABLE sso_setting ADD COLUMN createUserInIls int(11) DEFAULT 1',
+			]
+			
+		],
+		//sso_do_not_create_user_in_ils
+
 	];
 }
 
