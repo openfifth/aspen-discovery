dd251a59743d63268da3d48a6758c4c10611562b
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2025-02-07 10:49:58 +0000

    DIS-286 feat: BMJ BP results pagination

--- code/web/services/BmjBp/Results.php
+++ code/web/services/BmjBp/Results.php
@@ -15,12 +15,27 @@ class BmjBp_Results extends ResultsAction {
 		if (strlen($displayQuery ) > 20) {
 			$displayQuery  = substr($displayQuery , 0, 20) . '...';
 		}
+		if ($summary['resultTotal'] > 0) {
+			$this->implementPagination($searchObject, $summary);
+		}
 
 		$interface->assign('lookfor', $displayQuery);
 		$interface->assign('recordSet', $recordSet);
 		$interface->assign('subpage', '../Search/list-list.tpl');
 		$interface->assign('sectionLabel', 'BMJ Best Practice');
 		$this->display($summary['resultTotal'] > 0 ? '../BmjBp/list.tpl' : '../Search/list-none.tpl', $displayQuery , false, false);
+	}
+
+	function implementPagination($searchObject, $summary) {
+		global $interface;
+		$link = $searchObject->renderLinkPageTemplate();
+		$options = [
+			'totalItems' => $summary['resultTotal'],
+			'fileName' => $link,
+			'perPage' => $summary['perPage'],
+		];
+		$pager = new Pager($options);
+		$interface->assign('pageLinks', $pager->getLinks());
 	}
 
 	function getBreadcrumbs(): array {
