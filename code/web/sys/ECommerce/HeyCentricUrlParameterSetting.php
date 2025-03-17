adab9933974e9ed3449f7a649976422a3ca62b9d
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2025-03-17 12:01:41 +0000

    DIS-76: feat: display URL param sections
    On the HeyCentric Setting page, display a HeyCentric URL Parameter Settings section where, for each URL parameter stored in heycentric_url_param, the relevant input fields (see HeyCentricUrlParameterSetting) are displayed.
    
    Note:
    heycentric_setting has a m:m relationship with heycentric_url_parameter through heycentric_url_parameter_settings. In order to achieve the required form structure (on the settings page for each HeyCentric setting, a list of URL parameters for each of which a URL parameter setting is displayed), new data types where created ("[...]FromNestedSection"), allowing to access and update $propValue->$propName. This is later needed for each nested section's values to be displayed and updated.

---
+++ code/web/sys/ECommerce/HeyCentricUrlParameterSetting.php
@@ -0,0 +1,81 @@
+<?php
+require_once ROOT_DIR . '/sys/ECommerce/HeyCentricUrlParameter.php'; 
+
+class HeyCentricUrlParameterSetting extends DataObject {
+	public $__table = 'heycentric_url_parameter_setting';
+	public $id;
+	public $value;
+	public $heyCentricSettingId;
+	public $heyCentricUrlParameterId;
+	public $includeInUrl;
+	public $includeInHash;
+	public $kohaAdditionalField;
+
+	static function getObjectStructure($context = ''): array {
+		global $library;
+		$accountProfile = $library->getAccountProfile();
+		$catalogDriverName = trim($accountProfile->driver);
+		$catalogDriver = CatalogFactory::getCatalogConnectionInstance($catalogDriverName, $accountProfile);
+		$additionalFineFields = $catalogDriver->hasAdditionalFields() ? $catalogDriver->getAdditionalFieldNames('accountlines', null) : null;
+		$additionalDebitTypeFields = $catalogDriver->hasAdditionalFields() ? $catalogDriver->getAdditionalFieldNames('account_debit_types', null) : null;
+		$additionalLibraryBranchFields = $catalogDriver->hasAdditionalFields() ? $catalogDriver->getAdditionalFieldNames('branches', null) : null;
+		
+		$urlParam = new HeyCentricUrlParameter();
+		$urlParam = $urlParam->fetchAll();
+		
+		$structure = [];
+		
+		foreach ($urlParam as $param) {
+			$additionalFieldsWithDefault = ['none' => 'none'] + $additionalLibraryBranchFields; // Also applies to URL parameter that are not multiline and are tied to the general payment itself rather than to a specific line on that payment
+			$additionalFieldsWithDefault += $param->multiline ? $additionalFineFields + $additionalDebitTypeFields : []; // Only multiline URL parameters are tied to a specific fine (Koha accountline) and have the required unique identifier.
+			$propertyStructure = [
+				'id' => $param->id,
+				'property' => $param->name,
+				'type' => 'section',
+				'label' => $param->name,
+				'maxLength' => 10,
+				'properties' => [ 
+					$param->name . '_includeInUrl' => [
+						'property' => $param->name . '_includeInUrl',
+						'type' => 'checkboxFromNestedSection',
+						'label' => 'Include In URL',
+						'description' => 'Whether or not to to include this URL parameter in the HeyCentric payment URL.',
+						'hideInLists' => false,
+						'default' => false,
+					],  
+					$param->name . '_includeInHash' => [
+						'property' => $param->name . '_includeInHash',
+						'type' => 'checkboxFromNestedSection',
+						'label' => 'Include In Hash',
+						'description' => 'Whether or not to include this URL parameter in the HeyCentric payment URL hash.',
+						'hideInLists' => false,
+						'default' => false,
+					],
+					$param->name . '_value' => [
+						'property' => $param->name . '_value',
+						'type' => 'textFromNestedSection',
+						'label' => 'Value to assign to this URL parameter if known',
+						'required' => false,
+						'description' => $param->defaultValue ? 'Default: '. $param->defaultValue : 'If the parameter value is known and can be tied to a setting, please enter it here. Otherwise, please leave this blank.',
+						'hideInLists' => false,
+					], 
+					$param->name . '_kohaAdditionalField' => [
+						'property' => $param->name . '_kohaAdditionalField',
+						'type' => 'enumFromNestedSection',
+						'label' => 'Name of the matching Koha additional field if any',
+						'description' => 'If using a Koha additional field, select its name here. This dropdown only includes the Koha additional fields available to this URL parameter specifically, and may vary from one URL parameter to another.',
+						'values' => $additionalFieldsWithDefault,
+						'hideInLists' => false,
+					],
+				]				
+			];
+			
+			if ($additionalFieldsWithDefault === ['none' => 'none']) {
+				unset($propertyStructure['properties'][$param->name . '_kohaAdditionalField']);
+			}
+			$structure[] = $propertyStructure;
+		}
+		
+		return $structure;
+	}
+}
