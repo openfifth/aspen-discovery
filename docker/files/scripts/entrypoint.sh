b1f4706bcc09adc9343d7263b3dfc52fbdc3df28
Lucas Montoya <lucasmontoya@theke.io>
2025-03-21 17:39:49 -0300

    DIS-1226: Addapt entrypoint script for the new scenario
    - Create a new apache.sh entrypoint
    - Include apache as a new case within the entrypoint.sh

--- docker/files/scripts/entrypoint.sh
+++ docker/files/scripts/entrypoint.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
 set -e
 
+USER_NAME="www-data"
+DEFAULT_UID=33
+
 if [ "$#" -eq 0 ]; then
-    # Adjust permissions if required
-    if [[ ! -z "${LOCAL_USER_ID}" && "${LOCAL_USER_ID}" != "33" ]]; then
-    	echo "%   Setting www-data to UID=${LOCAL_USER_ID}"
-        usermod -o -u ${LOCAL_USER_ID} "www-data"
-        # Fix permissions due to UID change
-        chown -R "www-data" "/var/log/apache2"
-    fi
-    chown -R www-data /usr/local/aspen-discovery
-    exec /start.sh
+
+	echo "%   Starting container with UID=${LOCAL_USER_ID}"
+
+	if [[ ! -z "${LOCAL_USER_ID}" && "${LOCAL_USER_ID}" != "33" ]]; then
+		echo "%   Setting UID for ${USER_NAME} to ${LOCAL_USER_ID}"
+		usermod -o -u "$LOCAL_USER_ID" "$USER_NAME"
+	fi
+
+	if [[ -d /usr/local/aspen-discovery ]]; then
+		echo "%   Fixing ownership on /usr/local/aspen-discovery"
+		chown -R "$USER_NAME" /usr/local/aspen-discovery
+	fi
+
+	exec "/start.sh"
+
+
+elif [ "$1" = 'apache' ]; then
+
+	# Adjust permissions if required
+	if [[ ! -z "${LOCAL_USER_ID}" && "${LOCAL_USER_ID}" != "33" ]]; then
+		echo "%   Setting www-data to UID=${LOCAL_USER_ID}"
+		usermod -o -u ${LOCAL_USER_ID} "$USER_NAME"
+		# Fix permissions due to UID change
+		chown -R ${USER_NAME} "/var/log/apache2"
+	fi
+	chown -R "$USER_NAME" /usr/local/aspen-discovery
+	exec /apache.sh
+
 elif [ "$1" = 'cron' ]; then
-    # Adjust permissions if required
-    if [[ ! -z "${LOCAL_USER_ID}" && "${LOCAL_USER_ID}" != "33" ]]; then
-    	echo "%   Setting www-data to UID=${LOCAL_USER_ID}"
-        usermod -o -u ${LOCAL_USER_ID} "www-data" 
-    fi
-    chown -R www-data /usr/local/aspen-discovery
-    exec /cron.sh
+
+	# Adjust permissions if required
+	if [[ ! -z "${LOCAL_USER_ID}" && "${LOCAL_USER_ID}" != "33" ]]; then
+		echo "%   Setting www-data to UID=${LOCAL_USER_ID}"
+		usermod -o -u ${LOCAL_USER_ID} "$USER_NAME"
+	fi
+	chown -R ${USER_NAME} /usr/local/aspen-discovery
+	exec /cron.sh
+
 else
-    exec "$@"
+	exec "$@"
 fi
