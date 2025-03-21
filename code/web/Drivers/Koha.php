15fc5444bec654b310b762d34d83e2d750c7c316
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2025-03-21 10:44:11 +0000

    DIS-76: fix: use payment branch code
    When getting Koha additional field values that are to do with a specific fine (accountline:debit in Koha), and are tied to the branches table in the Koha database, use this branchcode as the record_id to get the value instead of the patron home location code.
    
    Note: patron home location (or library) is still used to determine which HeyCentric setting to use when building a HeyCentric payment URL.
    This is because payments are multiline, and since it cannot be assumed that each line will have been issued by the same libray or location, it is not currently possible to determine which setting to use using this information.

--- code/web/Drivers/Koha.php
+++ code/web/Drivers/Koha.php
@@ -2984,7 +2984,7 @@ class Koha extends AbstractIlsDriver {
 		return $fines;
 	}
 
-	public function getFineById(string $id, bool $includeAdditionalFieldValues = false, string $patronHomeLocationCode): array|null {
+	public function getFineById(string $id, bool $includeAdditionalFieldValues = false): array|null {
 		require_once ROOT_DIR . '/sys/Utils/StringUtils.php';
 
 		global $activeLanguage;
@@ -3032,6 +3032,7 @@ class Koha extends AbstractIlsDriver {
 			'amountOutstandingVal' => $fine['amountoutstanding'],
 			'amount' => $currencyFormatter->formatCurrency($fine['amount'], $currencyCode),
 			'amountOutstanding' => $currencyFormatter->formatCurrency($fine['amountoutstanding'], $currencyCode),
+			'branchCode' => $fine['branchcode'],
 		];
 
 		if (!$includeAdditionalFieldValues) {
@@ -3055,7 +3056,7 @@ class Koha extends AbstractIlsDriver {
 		}
 
 		foreach ($additionalLibraryFieldValues as $additionalLibraryFieldValue) {
-			if ($additionalLibraryFieldValue["record_id"] ==  $patronHomeLocationCode) {
+			if ($additionalLibraryFieldValue["record_id"] ==  $fine['branchcode']) {
 				$lowerCaseFieldName = strtolower($additionalLibraryFieldValue['field_name']);
 				$snakeCaseFieldName = str_replace(" ", "_", $lowerCaseFieldName);
 
