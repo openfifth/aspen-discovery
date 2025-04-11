4ea43a48aadada9eaeaefa27d113058678ac4ae2
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-04-11 20:20:02 +0100

    DIS-606: feat: update db with weight column
    Add a weight column to the ce_campaign_milestones table in the db
    
    interim

--- code/web/sys/DBMaintenance/version_updates/25.Q2.01.php
+++ code/web/sys/DBMaintenance/version_updates/25.Q2.01.php
@@ -30,6 +30,13 @@ function getUpdates25_Q2_01(): array {
 		], //add_placard_image_max_height_to_themes
 
 		//alexander - PTFS-Europe
+		'add_weight_to_campaign_milestones' => [
+			'title' => 'Add Weight To Campaign Milestones',
+			'description' => 'Add a weight column to campaign milestones to allow ordering',
+			'sql' => [
+				"ALTER TABLE ce_campaign_milestones ADD COLUMN weight int(11) NOT NULL DEFAULT 0",
+			],
+		], //DIS-606
 
 		//chloe - PTFS-Europe
 
