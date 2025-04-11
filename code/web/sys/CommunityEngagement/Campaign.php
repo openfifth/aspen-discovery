feaa8e135a5dc5c4ff1b9d0e332f706f0854a13e
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-04-11 20:21:34 +0100

    DIS-606: feat: Add weight to campaign milestones
    (cherry picked from commit f5d2294d8131384a29f7a5b9cd3f07148915dd0f)

--- code/web/sys/CommunityEngagement/Campaign.php
+++ code/web/sys/CommunityEngagement/Campaign.php
@@ -34,6 +34,7 @@ class Campaign extends DataObject {
 		$milestoneList = Milestone::getMilestoneList();
 		$milestoneStructure = CampaignMilestone::getObjectStructure($context);
 		unset($milestoneStructure['campaignId']);
+		unset($milestoneStructure['weight']);
 
 		$libraryList = Library::getLibraryList(false);
 		$patronTypeList = PType::getPatronTypeList();
@@ -270,6 +271,7 @@ class Campaign extends DataObject {
 			if (!empty($this->id)) {
 				$campaignMilestone = new CampaignMilestone();
 				$campaignMilestone->campaignId = $this->id;
+				$campaignMilestone->orderBy('weight');
 			   if ($campaignMilestone->find()) {
 					while ($campaignMilestone->fetch()) {
 						$this->_availableMilestones[$campaignMilestone->id] = clone($campaignMilestone);
