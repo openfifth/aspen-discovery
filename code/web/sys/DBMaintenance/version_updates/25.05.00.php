afbe11478c845281715be5d158eb637b83067c34
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-04-11 20:20:02 +0100

    DIS-606: feat: update db with weight column
    Add a weight column to the ce_campaign_milestones table in the db
    
    interim
    
    (cherry picked from commit 73086942c251c2bac84b7eedd03f85cb6d715702)

---
+++ code/web/sys/DBMaintenance/version_updates/25.05.00.php
@@ -0,0 +1,59 @@
+<?php
+
+function getUpdates25_05_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark - Grove
+
+		//katherine - Grove
+
+		//kirstien - Grove
+
+		//kodi - Grove
+
+		//Yanjun Li - ByWater
+
+		// Leo Stoyanov - BWS
+
+		//alexander - Open Fifth
+		'allow_filtering_of_linked_users_in_checkouts' => [
+			'title' => 'Allow Filtering of Linked Users in Checkouts',
+			'description' => 'Allow libraries the option of allowing users to filter their checkouts by linked user',
+			'sql' => [
+				'ALTER TABLE library ADD COLUMN allowFilteringOfLinkedAccountsInCheckouts TINYINT(1) DEFAULT 0',
+			],
+		], //allow_filtering_of_linked_users_in_checkouts
+		'allow_selecting_checkouts_to_export' => [
+			'title' => 'Allow Selecting Checkouts to Export',
+			'description' => 'Allow libraries the option of allowing users to export only selected checkouts',
+			'sql' => [
+				'ALTER TABLE library ADD COLUMN allowSelectingCheckoutsToExport TINYINT(1) DEFAULT 0'
+			],
+		], //allow_selecting_checkouts_to_export
+		'add_weight_to_campaign_milestones' => [
+			'title' => 'Add Weight To Campaign Milestones',
+			'description' => 'Add a weight column to campaign milestones to allow ordering',
+			'sql' => [
+				"ALTER TABLE ce_campaign_milestones ADD COLUMN weight int(11) NOT NULL DEFAULT 0",
+			],
+		],
+
+		//chloe - PTFS-Europe
+
+		//James Staub - Nashville Public Library
+
+		//Lucas Montoya - Theke Solutions
+
+		//other
+
+	];
+}
