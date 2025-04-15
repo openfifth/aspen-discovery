30bb12302692fb04f524726ef04b069962b025f1
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-04-15 10:39:25 +0100

    interim

--- code/web/services/API/UserAPI.php
+++ code/web/services/API/UserAPI.php
@@ -6645,7 +6645,7 @@ class UserAPI extends AbstractAPI {
 		$paginated = array_slice($campaigns, $offset, $pageSize);
 
 		$paginated = array_map(function($campaign) {
-			return (array)$campaign;
+			return get_object_vars($campaign);
 		}, $paginated);
 		
 		return [
