6dd0d59e0026bb75f159bc5d215f9774b08664c9
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-04-21 14:11:02 -0500

    DIS-679: Fixed "Usage Dashboard" breadcrumb and "Records Accessed Online" usage graphs.

--- code/web/services/SideLoads/UsageGraphs.php
+++ code/web/services/SideLoads/UsageGraphs.php
@@ -36,7 +36,7 @@ class SideLoads_UsageGraphs extends Admin_Admin {
 		$breadcrumbs = [];
 		$breadcrumbs[] = new Breadcrumb('/Admin/Home', 'Administration Home');
 		$breadcrumbs[] = new Breadcrumb('/Admin/Home#side_loads', 'Side Loads');
-		$breadcrumbs[] = new Breadcrumb('/SideLoads/UsageDashboard', 'Usage Dashboard');
+		$breadcrumbs[] = new Breadcrumb('/SideLoads/Dashboard', 'Usage Dashboard');
 		$breadcrumbs[] = new Breadcrumb('', 'Usage Graph');
 		return $breadcrumbs;
 	}
@@ -149,6 +149,7 @@ class SideLoads_UsageGraphs extends Admin_Admin {
 				$usage->instance = $instanceName;
 			}
 
+			$usage->whereAdd("sideloadId = $sideloadId");
 			$usage->selectAdd(null);
 			$usage->selectAdd();
 			$usage->selectAdd('year');
