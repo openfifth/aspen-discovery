e5f3a120be11b9660f96d889eeb3472755882b37
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-04-30 11:35:53 +0100

    fix: add permanentUrl db update
    Adds the 25.05.05 permanentUrl db update to 25.01.01 db
    updates. Resolve an indexing error where records where
    longer URLs were not indexed due to db constraints.

--- code/web/sys/DBMaintenance/version_updates/25.01.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.01.00.php
@@ -140,7 +140,13 @@ function getUpdates25_01_00(): array {
 
 		//alexander - PTFS-Europe
 
-		//chloe - PTFS-Europe
+    		//chloe - Open Fifth
+		'permanentUrl_allows_longer_strings' => [
+			'title' => 'PermanentUrl Allows For Longer Strings',
+			'description' => 'Allow for longer permanent URLs so that Open Archive records can be indexed without clashing with the length constraint',
+			'continueOnError' => false,
+			'sql' => ["ALTER TABLE open_archives_record MODIFY COLUMN permanentUrl VARCHAR(2048) NOT NULL"]
+		], // permanentUrl_allows_longer_strings
 
 		//James Staub - Nashville Public Library
 
