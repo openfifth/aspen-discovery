a354b012567934c645df989a10924d179a83e3ae
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-04-30 20:12:58 +0100

    DIS-249: feat: update library table in db
    Add the preferred name option to the name display dropdown in the
    library db

--- code/web/sys/DBMaintenance/version_updates/25.06.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.06.00.php
@@ -35,6 +35,14 @@ function getUpdates25_06_00(): array {
                 "ALTER TABLE user ADD COLUMN userPreferredName VARCHAR(256) NOT NULL",
             ]
         ], //add_preferred_name
+        'add_preferred_name_option_to_dropdown' => [
+            'title' => 'Add Preferred Name Option To Dropdown',
+            'description' => 'Add the preferred name option to the name display dropdown in the library.',
+            'continueOnError' => false,
+            'sql' => [
+                "ALTER TABLE library MODIFY COLUMN patronNameDisplayStyle ENUM('firstinitial_lastname','lastinitial_firstname','firstinitial_middleinitial_lastname','firstname_middleinitial_lastinitial', 'preferredname_lastinitial') DEFAULT 'firstinitial_lastname'",
+            ]
+        ], //add_preferred_name_option_to_dropdown
 
 		//chloe - Open Fifth
 
