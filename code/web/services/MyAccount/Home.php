6c1ff7378b3580a4b74453a9300a8945b922f265
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-05-01 17:17:26 +0100

    DIS-352: fix: Add null check
    Add null check to prevent an error if user has no home library

--- code/web/services/MyAccount/Home.php
+++ code/web/services/MyAccount/Home.php
@@ -10,7 +10,8 @@ class MyAccount_Home extends MyAccount {
 		// The script should only execute when a user is logged in, otherwise it calls Login.php
 		if (UserAccount::isLoggedIn()) {
 			$user = UserAccount::getLoggedInUser();
-			$homeLibraryCommunityEngagementHighlight = $user->getHomeLibrary()->highlightCommunityEngagement;
+			$homeLibrary = $user->getHomeLibrary();
+			$homeLibraryCommunityEngagementHighlight = $homeLibrary ? $homeLibrary->highlightCommunityEngagement : null;
 			// Check to see if the user has rated any titles
 			$interface->assign('hasRatings', $user->hasRatings());
 			$interface->assign('highlightCommunityEngagement', $homeLibraryCommunityEngagementHighlight);
