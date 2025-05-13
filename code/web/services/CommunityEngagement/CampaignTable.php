b4e2134571f6e0b0589b2da96c18811686254a1a
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-05-13 11:27:59 +0100

    DIS-797: Add permissions to CampaignTable

--- code/web/services/CommunityEngagement/CampaignTable.php
+++ code/web/services/CommunityEngagement/CampaignTable.php
@@ -107,7 +107,9 @@ class CommunityEngagement_CampaignTable extends Admin_Dashboard {
 	}
 
 	function canView(): bool {
-		return true;
+		return UserAccount::userHasPermission([
+			'View Community Engagement Dashboard',
+		]);
 	}
 
 	function getActiveAdminSection(): string
