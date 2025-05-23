b998954b4e7a21a45d30b5aef9dc2c707bb08519
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-05-23 15:50:16 +0100

    DIS-109: feat: Add db update for grapes templates
    int

--- code/web/sys/DBMaintenance/version_updates/25.07.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.07.00.php
@@ -31,6 +31,13 @@ function getUpdates25_07_00(): array {
 		// Laura Escamilla - ByWater Solutions
 
 		//alexander - Open Fifth
+		'add_grapes_templates_to_db' => [
+			'title' => 'Add Grapes Temaplates To DB',
+			'description' => 'Add Grapes templates to db',
+			'sql' => [
+				'addTemplateFromJson'
+			]
+		], //add_grapes_templates_to_db
 
 		//chloe - Open Fifth
 
@@ -41,4 +48,21 @@ function getUpdates25_07_00(): array {
 		//other
 
 	];
+}
+
+ function addTemplateFromJson(&$update) {
+	require_once ROOT_DIR . '/sys/WebBuilder/GrapesTemplate.php';
+
+	$jsonFile = './web_builder/templates.json';
+	if(file_exists($jsonFile)){
+		$jsonData = file_get_contents($jsonFile);
+		$jsonDecoded = json_decode($jsonData, true);
+		$templates = $jsonDecoded['templates'];
+
+		foreach($templates as $preMadeTemplate) {
+			$template = new GrapesTemplate();
+			$template->addTemplate($preMadeTemplate['templateName'], $preMadeTemplate['templateContent'], $preMadeTemplate['htmlData'] ?? '', $preMadeTemplate['cssData'] ?? '');
+		}
+		$update['success'] = true;
+	}
 }
