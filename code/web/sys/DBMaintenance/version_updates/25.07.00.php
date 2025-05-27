03889439ce10d5b31bade402e028cf341b6bd346
Jacob O'Mara <jacob.omara@openfifth.co.uk>
2025-05-27 12:02:01 +0100

    DIS-887: Add useragent tracking toggle to db

--- code/web/sys/DBMaintenance/version_updates/25.07.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.07.00.php
@@ -34,6 +34,15 @@ function getUpdates25_07_00(): array {
 
 		//chloe - Open Fifth
 
+		//Jacob - Open Fifth
+		'disable_user_agent_logging' => [
+			'title' => 'Disable User Agent Logging',
+			'description' => 'Add system variable to control user agent logging',
+			'sql' => [
+				"ALTER TABLE system_variables ADD COLUMN disable_user_agent_logging tinyint(1) DEFAULT 0",
+			]
+		], //disable_user_agent_logging
+
 		//James Staub - Nashville Public Library
 
 		//Lucas Montoya - Theke Solutions
