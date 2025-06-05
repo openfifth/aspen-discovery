e918816dc5faae375a606ad38113139884f7fa59
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-06-05 10:31:57 +0100

    DIS-927: fix: amend HeyCentric permission

---
+++ code/web/sys/DBMaintenance/version_updates/25.07.00.php
@@ -0,0 +1,54 @@
+<?php
+
+function getUpdates25_07_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark - Grove
+
+		//katherine - Grove
+
+		//kirstien - Grove
+
+		//kodi - Grove
+
+		//Mark - Grove
+
+		// Myranda - Grove
+
+		//Yanjun Li - ByWater
+
+		// Leo Stoyanov - BWS
+
+		// Laura Escamilla - ByWater Solutions
+
+		//alexander - Open Fifth
+
+		//chloe - Open Fifth
+		'move_heycentric_permission' => [
+			 'title' => 'Move HeyCentric Permission',
+			 'description' => 'Move the Administrer HeyCentric Settings permission into the existing eCommerce section',
+			 'continueOnError' => false,
+			 'sql' => [
+				"UPDATE permissions SET name='Administer HeyCentric', sectionName='eCommerce', description='Allows the user to administer the integration with HeyCentric <em>This has potential security and cost implications.</em>' WHERE name='Administer HeyCentric Settings' AND sectionName='ecommerce'",
+			 ],
+			 
+		 ], // move_heycentric_permission
+
+
+		//James Staub - Nashville Public Library
+
+		//Lucas Montoya - Theke Solutions
+
+		//other
+
+	];
+}
