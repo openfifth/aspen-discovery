f5f9fa520306cae291f0cb10676be20528d78550
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-06-12 16:09:44 +0100

    DIS-470: Update db

--- code/web/sys/DBMaintenance/version_updates/25.07.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.07.00.php
@@ -3,7 +3,12 @@
 function getUpdates25_07_00(): array {
 	$curTime = time();
 	return [
-	
-
+        'control_display_of_user_dropdown_in_community_engagement_admin_view' => [
+			'title' => 'Control User Select Type in Admin View',
+			'description' => 'Add options for how to select users in the admin view section',
+			'sql' => [
+				"ALTER TABLE library ADD COLUMN communityEngagementAdminUserSelect VARCHAR(20) DEFAULT 'dropdown'",
+			],
+		], //control_display_of_user_dropdown_in_community_engagement_admin_view
 	];
 }
