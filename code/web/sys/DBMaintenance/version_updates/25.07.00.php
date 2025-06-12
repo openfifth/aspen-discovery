e9506cacbd178d44a43487f1cbab93844cb87016
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-06-12 16:09:44 +0100

    DIS-470: Update db

--- code/web/sys/DBMaintenance/version_updates/25.07.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.07.00.php
@@ -103,6 +103,13 @@ function getUpdates25_07_00(): array {
 				'addTemplateFromJson'
 			]
 		], //add_grapes_templates_to_db
+		'control_display_of_user_dropdown_in_community_engagement_admin_view' => [
+			'title' => 'Control User Select Type in Admin View',
+			'description' => 'Add options for how to select users in the admin view section',
+			'sql' => [
+				"ALTER TABLE library ADD COLUMN communityEngagementAdminUserSelect VARCHAR(20) DEFAULT 'dropdown'",
+			],
+		], //control_display_of_user_dropdown_in_community_engagement_admin_view
 
 		//chloe - Open Fifth
 		'move_heycentric_permission' => [
