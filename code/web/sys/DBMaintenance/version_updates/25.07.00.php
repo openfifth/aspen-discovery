a49ccca611cb1bbfe7625cfb318f2cbe6af584d0
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-06-13 10:43:29 +0100

    DIS-470: feat: Add option to db
    Add the ability to control whether users from all libraries or only
    those who have their home location id set to the current library are
    displayed when searching by user in the admin view.

--- code/web/sys/DBMaintenance/version_updates/25.07.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.07.00.php
@@ -10,5 +10,12 @@ function getUpdates25_07_00(): array {
 				"ALTER TABLE library ADD COLUMN communityEngagementAdminUserSelect VARCHAR(20) DEFAULT 'dropdown'",
 			],
 		], //control_display_of_user_dropdown_in_community_engagement_admin_view
+        'display_only_users_from_current_library_in_user_search_admin_view' => [
+			'title' => 'Display Only Users From Current Library in User Search Admin View',
+			'description' => 'Add option to display users from all libraries or only the current library location when searching by user in Admin View',
+			'sql' => [
+				"ALTER TABLE library ADD COLUMN displayOnlyUsersForLocationInUserAdmin TINYINT(1) DEFAULT 0",
+			],
+		], //display_only_users_from_current_library_in_user_search_admin_view
 	];
 }
