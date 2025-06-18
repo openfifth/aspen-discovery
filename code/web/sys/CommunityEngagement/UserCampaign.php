2d2769830d5860b0ea90dfc2580fbfc85460d4f7
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-06-18 16:04:47 +0100

    DIS-978: style: tabs vs spaces
    Signed-off-by: AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>

--- code/web/sys/CommunityEngagement/UserCampaign.php
+++ code/web/sys/CommunityEngagement/UserCampaign.php
@@ -5,17 +5,17 @@
 	require_once ROOT_DIR . '/sys/CommunityEngagement/CampaignMilestoneUsersProgress.php';
 
 class UserCampaign extends DataObject {
-    public $__table = 'ce_user_campaign';
-    public $id;
-    public $userId;
-    public $campaignId;
-    public $enrollmentDate;
-    public $unenerollmentDate;
-    public $completed;
-    public $rewardGiven;
-    public $optInToCampaignLeaderboard;
-    public $optInToCampaignEmailNotifications;
-    public $campaignCompleteEmailSent;
+	public $__table = 'ce_user_campaign';
+	public $id;
+	public $userId;
+	public $campaignId;
+	public $enrollmentDate;
+	public $unenerollmentDate;
+	public $completed;
+	public $rewardGiven;
+	public $optInToCampaignLeaderboard;
+	public $optInToCampaignEmailNotifications;
+	public $campaignCompleteEmailSent;
 
 	public static function getObjectStructure($context = ''): array {
 		return [
@@ -54,29 +54,29 @@ class UserCampaign extends DataObject {
 				'type' => 'checkbox',
 				'label' => 'Campaign Complete',
 				'description' => 'Whether or not the campaign is complete',
-                'default' => false,
-            ],
-            'rewardGiven' => [
-                'property' => 'rewardGiven',
-                'type' => 'checkbox',
-                'label' => 'Reward Given',
-                'description' => 'Whether or not the reward for completing the campaign has been given',
-                'default' => false,
-            ],
-            'optInToCampaignLeaderboard' => [
-                'property' => 'optInToCampaignLeaderboard',
-                'type' => 'checkbox',
-                'label' => 'Opt In To Campaign Leaderboard',
-                'description' => 'Whether or not to opt into the being displayed on the leaderboard for this campaign',
-            ],
-            'optInToCampaignEmailNotifications' => [
-                'property' => 'optInToCampaignEmailNotifications',
-                'type' => 'checkbox',
-                'label' => 'Opt In To Campaign Notification Emails',
-                'description' => 'Whether or not to opt in to email notifications for this campaign',
-            ],
-        ];
-    }
+				'default' => false,
+			],
+			'rewardGiven' => [
+				'property' => 'rewardGiven',
+				'type' => 'checkbox',
+				'label' => 'Reward Given',
+				'description' => 'Whether or not the reward for completing the campaign has been given',
+				'default' => false,
+			],
+			'optInToCampaignLeaderboard' => [
+				'property' => 'optInToCampaignLeaderboard',
+				'type' => 'checkbox',
+				'label' => 'Opt In To Campaign Leaderboard',
+				'description' => 'Whether or not to opt into the being displayed on the leaderboard for this campaign',
+			],
+			'optInToCampaignEmailNotifications' => [
+				'property' => 'optInToCampaignEmailNotifications',
+				'type' => 'checkbox',
+				'label' => 'Opt In To Campaign Notification Emails',
+				'description' => 'Whether or not to opt in to email notifications for this campaign',
+			],
+		];
+	}
 
 	//Check if the user has completed the campaign
 	public function checkCompletionStatus() {
@@ -115,176 +115,176 @@ class UserCampaign extends DataObject {
 		return $milestoneCompletionStatus;
 	}
 
-     /**
-     * Calculate the total number of completed milestones for a user
-     * @param int $userId
-     * @return int
-     */
-    public function calculateUserCompletedMilestones($userId) {
-        $userCampaign = new UserCampaign();
-        $userEnrolledCampaigns = [];
-        $userCampaign->whereAdd("userId = '$userId'");
-        $userCampaign->find();
-        while ($userCampaign->fetch()) {
-            $userEnrolledCampaigns[] = clone $userCampaign;
-        }
-        $totalCompletedMilestones = 0;
-        foreach ($userEnrolledCampaigns as $userEnrolledCampaign) {
-            $milestoneCompletionStatus = $userEnrolledCampaign->checkMilestoneCompletionStatus();
-            $completedMilestones = array_filter($milestoneCompletionStatus, function($status) {
-                return $status === true;
-            });
-            //Add the completed milestones count to the total 
-            $totalCompletedMilestones += count($completedMilestones);
-            return $totalCompletedMilestones;
-        }
-    }
-    /**
-     * Calculate the user's rank based on completed milestones
-     * @param int $userId
-     * @return int
-     */
-    public function getUserRank($userId) {
-        $campaign = new Campaign();
-        $allUsers = $campaign->getAllUsersInCampaigns();
-        $userCompletedMilestones = [];
-        foreach ($allUsers as $user) {
-            $totalCompletedMilestones = $this->calculateUserCompletedMilestones($userId);
-            $userCompletedMilestones[$user] = $totalCompletedMilestones;
-        }
-        arsort($userCompletedMilestones);
-        $rank = 1;
-        $previousCompletedMilestones = null;
-        $userRank = null;
-        foreach ($userCompletedMilestones as $userId => $completedMilestones) {
-            if ($completedMilestones != $previousCompletedMilestones) {
-                $rank = $rank;
-            }
-            if ($user == $userId) {
-                $userRank = $rank;
-                break;
-            }
-            $previousCompletedMilestones = $completedMilestones;
-            $rank++;
-        }
-        return $userRank;
-    }
+	 /**
+	 * Calculate the total number of completed milestones for a user
+	 * @param int $userId
+	 * @return int
+	 */
+	public function calculateUserCompletedMilestones($userId) {
+		$userCampaign = new UserCampaign();
+		$userEnrolledCampaigns = [];
+		$userCampaign->whereAdd("userId = '$userId'");
+		$userCampaign->find();
+		while ($userCampaign->fetch()) {
+			$userEnrolledCampaigns[] = clone $userCampaign;
+		}
+		$totalCompletedMilestones = 0;
+		foreach ($userEnrolledCampaigns as $userEnrolledCampaign) {
+			$milestoneCompletionStatus = $userEnrolledCampaign->checkMilestoneCompletionStatus();
+			$completedMilestones = array_filter($milestoneCompletionStatus, function($status) {
+				return $status === true;
+			});
+			//Add the completed milestones count to the total 
+			$totalCompletedMilestones += count($completedMilestones);
+			return $totalCompletedMilestones;
+		}
+	}
+	/**
+	 * Calculate the user's rank based on completed milestones
+	 * @param int $userId
+	 * @return int
+	 */
+	public function getUserRank($userId) {
+		$campaign = new Campaign();
+		$allUsers = $campaign->getAllUsersInCampaigns();
+		$userCompletedMilestones = [];
+		foreach ($allUsers as $user) {
+			$totalCompletedMilestones = $this->calculateUserCompletedMilestones($userId);
+			$userCompletedMilestones[$user] = $totalCompletedMilestones;
+		}
+		arsort($userCompletedMilestones);
+		$rank = 1;
+		$previousCompletedMilestones = null;
+		$userRank = null;
+		foreach ($userCompletedMilestones as $userId => $completedMilestones) {
+			if ($completedMilestones != $previousCompletedMilestones) {
+				$rank = $rank;
+			}
+			if ($user == $userId) {
+				$userRank = $rank;
+				break;
+			}
+			$previousCompletedMilestones = $completedMilestones;
+			$rank++;
+		}
+		return $userRank;
+	}
 
-    public function checkAndHandleCampaignCompletion($userId, $campaignId) {
-        require_once ROOT_DIR . '/sys/Email/EmailTemplate.php';
-        global $logger;
+	public function checkAndHandleCampaignCompletion($userId, $campaignId) {
+		require_once ROOT_DIR . '/sys/Email/EmailTemplate.php';
+		global $logger;
 
-        $userCampaign = new UserCampaign();
-        $userCampaign->userId = $userId;
-        $userCampaign->campaignId = $campaignId;
+		$userCampaign = new UserCampaign();
+		$userCampaign->userId = $userId;
+		$userCampaign->campaignId = $campaignId;
 
-        if ($userCampaign->find(true)) {
+		if ($userCampaign->find(true)) {
 
-                $user = new User();
-                $user->id = $userId;
-                if (!$user->find(true)) {
-                    return;
-                }
+				$user = new User();
+				$user->id = $userId;
+				if (!$user->find(true)) {
+					return;
+				}
 
-                $campaign = new Campaign();
-                $campaign->id = $campaignId;
-                if (!$campaign->find(true)) {
-                    return;
-                }
-                $campaignName = $campaign->name;
-                $isCampaignComplete = $this->checkCompletionStatus();
-                if ($isCampaignComplete) {
-                    $userCampaign->completed = 1;
-                    $userCampaign->update();
-                }
+				$campaign = new Campaign();
+				$campaign->id = $campaignId;
+				if (!$campaign->find(true)) {
+					return;
+				}
+				$campaignName = $campaign->name;
+				$isCampaignComplete = $this->checkCompletionStatus();
+				if ($isCampaignComplete) {
+					$userCampaign->completed = 1;
+					$userCampaign->update();
+				}
 
-                if ($userCampaign->completed == 1) {
+				if ($userCampaign->completed == 1) {
 
-                    $library = $user->getHomeLibrary();
-                    if ($library->sendStaffEmailOnCampaignCompletion == 1) {
-                        $emailTemplate = EmailTemplate::getActiveTemplate('staffCampaignComplete');
-                        
-                        if (!$emailTemplate) {
-                            $logger->log("No active email template found for 'campaignComplete'", Logger::LOG_ERROR);
-                        }
-                        if ($emailTemplate) {
-                            $parameters = [
-                                'user' => $user,
-                                'campaignName' => $campaignName,
-                                'library' => $library,
-                            ];
-                            try {
-                                $emailTemplate->sendEmail($library->campaignCompletionNewEmail, $parameters);
-                            } catch (Exception $e) {
-                                $logger->log("Exception while sending email to {$library->campaignCompletionNewEmail}: " . $e->getMessage(), Logger::LOG_ERROR);
-                            }
-                        }
-                    }
-                    if ($userCampaign->optInToCampaignEmailNotifications == 1 && $userCampaign->campaignCompleteEmailSent == 0) {
-                        $emailTemplate = EmailTemplate::getActiveTemplate('campaignComplete');
-                        if ($emailTemplate) {
-                            $parameters = [
-                                'user' => $user,
-                                'campaignName' => $campaignName,
-                                'library' => $user->getHomeLibrary(),
-                            ];
-                            try {
-                                $emailTemplate->sendEmail($user->email, $parameters);
-                                $userCampaign->campaignCompleteEmailSent = 1;
-                                $userCampaign->update();
-                            } catch (Exception $e) {
-                                $logger->log("Exception while sending email to {$user->email}: " . $e->getMessage(), Logger::LOG_ERROR);
-                            }
-                        }
-                    }
-                }
-                $milestone = new CampaignMilestone();
-                $milestone->campaignId = $campaignId;
-                $milestones = [];
+					$library = $user->getHomeLibrary();
+					if ($library->sendStaffEmailOnCampaignCompletion == 1) {
+						$emailTemplate = EmailTemplate::getActiveTemplate('staffCampaignComplete');
+						
+						if (!$emailTemplate) {
+							$logger->log("No active email template found for 'campaignComplete'", Logger::LOG_ERROR);
+						}
+						if ($emailTemplate) {
+							$parameters = [
+								'user' => $user,
+								'campaignName' => $campaignName,
+								'library' => $library,
+							];
+							try {
+								$emailTemplate->sendEmail($library->campaignCompletionNewEmail, $parameters);
+							} catch (Exception $e) {
+								$logger->log("Exception while sending email to {$library->campaignCompletionNewEmail}: " . $e->getMessage(), Logger::LOG_ERROR);
+							}
+						}
+					}
+					if ($userCampaign->optInToCampaignEmailNotifications == 1 && $userCampaign->campaignCompleteEmailSent == 0) {
+						$emailTemplate = EmailTemplate::getActiveTemplate('campaignComplete');
+						if ($emailTemplate) {
+							$parameters = [
+								'user' => $user,
+								'campaignName' => $campaignName,
+								'library' => $user->getHomeLibrary(),
+							];
+							try {
+								$emailTemplate->sendEmail($user->email, $parameters);
+								$userCampaign->campaignCompleteEmailSent = 1;
+								$userCampaign->update();
+							} catch (Exception $e) {
+								$logger->log("Exception while sending email to {$user->email}: " . $e->getMessage(), Logger::LOG_ERROR);
+							}
+						}
+					}
+				}
+				$milestone = new CampaignMilestone();
+				$milestone->campaignId = $campaignId;
+				$milestones = [];
 
-                if ($milestone->find()) {
-                    while ($milestone->fetch()) {
-                        $milestoneObj = new Milestone();
-                        $milestoneObj->id = $milestone->milestoneId;
-                        if ($milestoneObj->find(true)) {
-                            $milestone->name = $milestoneObj->name;
-                        }
-                        $milestones[] = clone $milestone;
-                    }
-                }
+				if ($milestone->find()) {
+					while ($milestone->fetch()) {
+						$milestoneObj = new Milestone();
+						$milestoneObj->id = $milestone->milestoneId;
+						if ($milestoneObj->find(true)) {
+							$milestone->name = $milestoneObj->name;
+						}
+						$milestones[] = clone $milestone;
+					}
+				}
 
-                foreach ($milestones as $milestone) {
-                    $milestoneProgress = new CampaignMilestoneUsersProgress();
-                    $milestoneProgress->userId = $userId;
-                    $milestoneProgress->ce_milestone_id = $milestone->milestoneId;
-                    $milestoneProgress->ce_campaign_id = $campaignId;
+				foreach ($milestones as $milestone) {
+					$milestoneProgress = new CampaignMilestoneUsersProgress();
+					$milestoneProgress->userId = $userId;
+					$milestoneProgress->ce_milestone_id = $milestone->milestoneId;
+					$milestoneProgress->ce_campaign_id = $campaignId;
 
-                    if ($milestoneProgress->find(true)) {
+					if ($milestoneProgress->find(true)) {
 
-                        if ($milestoneProgress->progress >= $milestone->goal && !$milestoneProgress->milestoneCompleteEmailSent){
+						if ($milestoneProgress->progress >= $milestone->goal && !$milestoneProgress->milestoneCompleteEmailSent){
 
-                            $emailTemplate = EmailTemplate::getActiveTemplate('milestoneComplete');
+							$emailTemplate = EmailTemplate::getActiveTemplate('milestoneComplete');
 
-                            if ($emailTemplate) {
-                                $parameters = [
-                                    'user' => $user,
-                                    'campaignName' => $campaign->name,
-                                    'milestoneName' => $milestone->name,
-                                    'library' => $user->getHomeLibrary(),
-                                ];
+							if ($emailTemplate) {
+								$parameters = [
+									'user' => $user,
+									'campaignName' => $campaign->name,
+									'milestoneName' => $milestone->name,
+									'library' => $user->getHomeLibrary(),
+								];
 
-                                try {
-                                    $emailTemplate->sendEmail($user->email, $parameters);
-                                    $milestoneProgress->milestoneCompleteEmailSent = 1;
-                                    $milestoneProgress->update();
-                                } catch (Exception $e) {
-                                    $logger->log("Error sending milestone email to {$user->email}: " . $e->getMessage(), Logger::LOG_ERROR);
-                                }
-                            }
-                        }
-                    }
-                }
-        }
-    }
+								try {
+									$emailTemplate->sendEmail($user->email, $parameters);
+									$milestoneProgress->milestoneCompleteEmailSent = 1;
+									$milestoneProgress->update();
+								} catch (Exception $e) {
+									$logger->log("Error sending milestone email to {$user->email}: " . $e->getMessage(), Logger::LOG_ERROR);
+								}
+							}
+						}
+					}
+				}
+		}
+	}
 
 }
