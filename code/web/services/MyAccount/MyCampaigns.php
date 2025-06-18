9c3fb87a4f5ba02e835ed100975b95a99b8b2b30
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-06-18 15:27:14 +0100

    DIS-991: style: tabs vs spaces

--- code/web/services/MyAccount/MyCampaigns.php
+++ code/web/services/MyAccount/MyCampaigns.php
@@ -12,21 +12,21 @@ class MyCampaigns extends MyAccount {
 	function launch() {
 		global $interface;
 		global $library;
-        global $enabledModules;
+		global $enabledModules;
 
-        $campaign = new Campaign();
-        //Get User
-        $userId = $this->getUserId();
-        $interface->assign('userId', $userId);
+		$campaign = new Campaign();
+		//Get User
+		$userId = $this->getUserId();
+		$interface->assign('userId', $userId);
 
-        $hasLinkedAccounts = UserAccount::hasLinkedUsers();
-        $interface->assign('hasLinkedAccounts', $hasLinkedAccounts);
+		$hasLinkedAccounts = UserAccount::hasLinkedUsers();
+		$interface->assign('hasLinkedAccounts', $hasLinkedAccounts);
 
-        $linkedCampaigns = $campaign->getLinkedUserCampaigns($userId);
-        $interface->assign('linkedCampaigns', $linkedCampaigns);
+		$linkedCampaigns = $campaign->getLinkedUserCampaigns($userId);
+		$interface->assign('linkedCampaigns', $linkedCampaigns);
 
-        $webBuilderEnabled = array_key_exists('Web Builder', $enabledModules);
-        $interface->assign('webBuilderEnabled', $webBuilderEnabled);
+		$webBuilderEnabled = array_key_exists('Web Builder', $enabledModules);
+		$interface->assign('webBuilderEnabled', $webBuilderEnabled);
 		//Get Campaigns
 		$campaignList = $campaign->getCampaigns();
 		$interface->assign('campaignList', $campaignList);
@@ -35,17 +35,17 @@ class MyCampaigns extends MyAccount {
 		$pastCampaigns = $campaign->getPastCampaigns($userId);
 		$interface->assign('pastCampaigns', $pastCampaigns);
 
-        $campaignLeaderboardDisplay = $this->userLeaderboardButtonDisplay();
-        $interface->assign('campaignLeaderboardDisplay', $campaignLeaderboardDisplay);
+		$campaignLeaderboardDisplay = $this->userLeaderboardButtonDisplay();
+		$interface->assign('campaignLeaderboardDisplay', $campaignLeaderboardDisplay);
 
-        $url = $this->getBaseUrl();
-        $interface->assign('url', $url);
+		$url = $this->getBaseUrl();
+		$interface->assign('url', $url);
 
-        $userCanAdvertise = $this->userCanAdvertise();
-        $interface->assign('userCanAdvertise', $userCanAdvertise);
+		$userCanAdvertise = $this->userCanAdvertise();
+		$interface->assign('userCanAdvertise', $userCanAdvertise);
 
-        $displayCampaignLeaderboard = $library->displayCampaignLeaderboard;
-        $interface->assign('displayCampaignLeaderboard', $displayCampaignLeaderboard);
+		$displayCampaignLeaderboard = $library->displayCampaignLeaderboard;
+		$interface->assign('displayCampaignLeaderboard', $displayCampaignLeaderboard);
 
 		$this->display('../MyAccount/myCampaigns.tpl', 'My Campaigns');
 	}
@@ -56,37 +56,37 @@ class MyCampaigns extends MyAccount {
 		return $userId;
 	}
 
-    function userLeaderboardButtonDisplay() {
-        global $library;
-        $user = UserAccount::getLoggedInUser();
-        if ($user->getHomeLibrary() != null){
-            $userLibrary = $user->getHomeLibrary();
-            $campaignLeaderboardDisplay = $userLibrary->campaignLeaderboardDisplay;
-        } else {
-            $campaignLeaderboardDisplay = $library->campaignLeaderboardDisplay;
-        }
-        return $campaignLeaderboardDisplay;
-    }
+	function userLeaderboardButtonDisplay() {
+		global $library;
+		$user = UserAccount::getLoggedInUser();
+		if ($user->getHomeLibrary() != null){
+			$userLibrary = $user->getHomeLibrary();
+			$campaignLeaderboardDisplay = $userLibrary->campaignLeaderboardDisplay;
+		} else {
+			$campaignLeaderboardDisplay = $library->campaignLeaderboardDisplay;
+		}
+		return $campaignLeaderboardDisplay;
+	}
 
-    public function getBaseUrl(): string {
-        global $configArray;
-        return $configArray['Site']['url'];
-    }
+	public function getBaseUrl(): string {
+		global $configArray;
+		return $configArray['Site']['url'];
+	}
 
-    public function userCanAdvertise() {
-        $user = UserAccount::getActiveUserObj();
-        if ($user->isAspenAdminUser() || $user->isUserAdmin()){
-            return true;
-        }
-    }
+	public function userCanAdvertise() {
+		$user = UserAccount::getActiveUserObj();
+		if ($user->isAspenAdminUser() || $user->isUserAdmin()){
+			return true;
+		}
+	}
 
 
 
-    //TODO:: Write a function that uses the milestone id for each progress bar to use the ce_milestone_progress_entries table and 
-    //get information about which books were checked out and count towards the milestone. 
-    function getBreadcrumbs(): array
-    {
-        $breadcrumbs = [];
+	//TODO:: Write a function that uses the milestone id for each progress bar to use the ce_milestone_progress_entries table and 
+	//get information about which books were checked out and count towards the milestone. 
+	function getBreadcrumbs(): array
+	{
+		$breadcrumbs = [];
 		$breadcrumbs[] = new Breadcrumb('/MyAccount/Home', 'Your Account');
 		$breadcrumbs[] = new Breadcrumb('', 'Campaigns');
 		return $breadcrumbs;
