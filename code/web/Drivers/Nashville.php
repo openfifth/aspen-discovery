6476c56a40c062925bdb1a97f1b83b674d337146
myr-onl <140292626+myr-onl@users.noreply.github.com>
2025-06-24 13:06:21 -0400

    Finish updating msgFeePaid calls, add release note
    Signed-off-by: Chloé Zermatten <chloe.zermatten@openfifth.co.uk>

--- code/web/Drivers/Nashville.php
+++ code/web/Drivers/Nashville.php
@@ -281,7 +281,7 @@ class Nashville extends CarlX {
 	protected function feePaidViaSIP($SIP2FeeType = '01', $pmtType = '02', $pmtAmount, $curType = 'USD', $feeId = '', $transId = '', $patronId = ''): array {
 		$mySip = $this->initSIPConnection();
 		if (!is_null($mySip)) {
-			$in = $mySip->msgFeePaid($SIP2FeeType, $pmtType, $pmtAmount, $curType, $feeId, $transId, $patronId);
+			$in = $mySip->msgFeePaid($pmtAmount, $SIP2FeeType, $pmtType, $curType, $feeId, $transId, $patronId);
 			$msg_result = $mySip->get_message($in);
 			ExternalRequestLogEntry::logRequest('carlx.feePaid', 'SIP2', $mySip->hostname . ':' . $mySip->port, [], $in, 0, $msg_result, []);
 			if (preg_match("/^38/", $msg_result)) {
