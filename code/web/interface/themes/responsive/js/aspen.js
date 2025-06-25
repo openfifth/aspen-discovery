17534e89ebf1adf7f0db82ba496c25a29df27b31
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-06-25 18:58:58 +0100

    DIS-470: refactor: remove users cache

--- code/web/interface/themes/responsive/js/aspen.js
+++ code/web/interface/themes/responsive/js/aspen.js
@@ -18903,13 +18903,7 @@ AspenDiscovery.CommunityEngagement = function() {
 			}
 			
 		},
-		usersCache: null,
 		getLibraryUsers: function (callback) {
-			if (AspenDiscovery.CommunityEngagement.usersCache) {
-				callback(this.usersCache);
-				return;
-			}
-
 			var url = Globals.path + "/CommunityEngagement/AJAX";
 			var params = {
 				method: 'getLibraryUsers',
@@ -18917,7 +18911,6 @@ AspenDiscovery.CommunityEngagement = function() {
 
 			$.getJSON(url, params, function (data) {
 				if (data.success && data.users) {
-					AspenDiscovery.CommunityEngagement.usersCache = data.users;
 					callback(data.users);
 				} else{
 					callback([]);
@@ -19137,8 +19130,6 @@ AspenDiscovery.CommunityEngagement = function() {
 				success: function (data) {
 					if (data.success) {
 						AspenDiscovery.showMessage(data.title, data.message);
-						AspenDiscovery.CommunityEngagement.usersCache = null;
-						// AspenDiscovery.CommunityEngagement.getLibraryUsers();
 						AspenDiscovery.CommunityEngagement.getLibraryUsers(function(users) {
 							if ($('#user_id').length > 0) {
 								const $dropdown = $('#user_id');
