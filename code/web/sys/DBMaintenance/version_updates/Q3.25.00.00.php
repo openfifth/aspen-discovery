8c04b28535c38e7c0f5f351843a5f68729af6aa6
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-07-09 15:42:22 +0100

    RMAINT: db updates

--- code/web/sys/DBMaintenance/version_updates/Q3.25.00.00.php
+++ code/web/sys/DBMaintenance/version_updates/Q3.25.00.00.php
@@ -34,6 +34,36 @@ function getUpdatesQ3_25_00_00(): array {
 		], //remove_starRating_from_overdrive_api_product_metadata
 
 		// Leo Stoyanov - BWS
+		'enable_web_builder_for_libraries_using_it' => [
+			'title' => 'Enable Web Builder for Libraries Using Web Builder Features',
+			'description' => 'Enable Web Builder only for libraries that have associated Web Builder content (resources, pages, forms, etc.).',
+			'continueOnError' => true,
+			'sql' => [
+				"UPDATE library SET enableWebBuilder = 1 
+				WHERE libraryId IN (
+					-- Libraries with Web Resources
+					SELECT DISTINCT libraryId FROM library_web_builder_resource
+					UNION
+					-- Libraries with Basic Pages  
+					SELECT DISTINCT libraryId FROM library_web_builder_basic_page
+					UNION
+					-- Libraries with Portal Pages (Custom Pages)
+					SELECT DISTINCT libraryId FROM library_web_builder_portal_page
+					UNION
+					-- Libraries with Custom Web Resource Pages
+					SELECT DISTINCT libraryId FROM library_web_builder_custom_web_resource_page
+					UNION
+					-- Libraries with Custom Forms
+					SELECT DISTINCT libraryId FROM library_web_builder_custom_form
+					UNION
+					-- Libraries with Grapes Pages
+					SELECT DISTINCT libraryId FROM library_web_builder_grapes_page
+					UNION
+					-- Libraries with Quick Polls
+					SELECT DISTINCT libraryId FROM library_web_builder_quick_poll
+				)",
+			],
+		],
 
 		// Laura Escamilla - ByWater Solutions
 
