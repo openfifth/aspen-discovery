faeedf7a6d970e665097e29ed82ec56926e16073
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-07-09 14:09:31 +0100

    RMAINT: Add db file

---
+++ code/web/sys/DBMaintenance/version_updates/25.07.00.php
@@ -0,0 +1,16 @@
+<?php
+
+function getUpdates25_07_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+	];
+}
