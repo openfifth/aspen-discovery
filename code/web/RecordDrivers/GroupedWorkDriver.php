3d674ec04cd8da8c82d6545afb471ce395e87e00
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-07-24 11:10:18 -0500

    DIS-131: Fix Staff View to actually load information when set to open by default.
    Signed-off-by: Chloé Zermatten <chloe.zermatten@openfifth.co.uk>

--- code/web/RecordDrivers/GroupedWorkDriver.php
+++ code/web/RecordDrivers/GroupedWorkDriver.php
@@ -1571,11 +1571,7 @@ class GroupedWorkDriver extends IndexRecordDriver {
 		if ($interface->getVariable('showStaffView')) {
 			$moreDetailsOptions['staff'] = [
 				'label' => 'Staff View',
-				'onShow' => "AspenDiscovery.GroupedWork.getStaffView('{$this->getPermanentId()}');",
-				'body' => '<div id="staffViewPlaceHolder">' . translate([
-						'text' => 'Loading Staff View.',
-						'isPublicFacing' => true,
-					]) . '</div>',
+				'body' => $interface->fetch($this->getStaffView()),
 			];
 		}
 
