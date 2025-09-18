096afb674cc9f83add287d1fcb5bb6dd34e44ac4
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-09-18 09:59:55 -0500

    DIS-1161: Add indexes on publicationDateId and callNumberId for faster User List sorting.

--- code/web/sys/DBMaintenance/version_updates/25.10.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.10.00.php
@@ -25,6 +25,15 @@ function getUpdates25_10_00(): array {
 		//Yanjun Li - ByWater
 
 		// Leo Stoyanov - BWS
+		'add_indexes_for_more_user_list_sort_options' => [
+			'title' => 'Add Indexes For More User List Sort Options',
+			'description' => 'Add indexes idx_publicationDateId and idx_callNumberId for faster User List sorting.',
+			'continueOnError' => false,
+			'sql' => [
+				'CREATE INDEX idx_publicationDateId ON grouped_work_records(publicationDateId)',
+				'CREATE INDEX idx_callNumberId ON grouped_work_record_items (callNumberId)'
+			],
+		], // add_indexes_for_more_user_list_sort_options
 
 		//alexander - Open Fifth
 
