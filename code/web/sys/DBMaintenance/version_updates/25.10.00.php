056a0bd860e9d3d3173e45426b013221965dfbff
Mark Noble <mdnoble73@gmail.com>
2025-09-18 15:11:20 -0600

    DIS-1335 Configure if 896-899 are indexed as series

--- code/web/sys/DBMaintenance/version_updates/25.10.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.10.00.php
@@ -13,6 +13,17 @@ function getUpdates25_10_00(): array {
 		 ], //name*/
 
 		//mark - Grove
+		'addOptionsForIndexing896To899AsSeries' => [
+			'title' => 'Add Options For Indexing 896 To 899 As Series',
+			'description' => 'Add Options For Indexing 896 To 899 As Series',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE indexing_profiles ADD COLUMN index896asSeries TINYINT(1) DEFAULT 1',
+				'ALTER TABLE indexing_profiles ADD COLUMN index897asSeries TINYINT(1) DEFAULT 1',
+				'ALTER TABLE indexing_profiles ADD COLUMN index898asSeries TINYINT(1) DEFAULT 1',
+				'ALTER TABLE indexing_profiles ADD COLUMN index899asSeries TINYINT(1) DEFAULT 1'
+			]
+		], //addOptionsForIndexing896To899AsSeries
 
 		//katherine - Grove
 
