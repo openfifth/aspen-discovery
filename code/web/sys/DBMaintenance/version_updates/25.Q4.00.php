aa0deb7db92044024ab5c173277511c5589c88fd
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-09-23 13:45:17 -0500

    RMAINT: Initial commit, including release notes and DB update file.

---
+++ code/web/sys/DBMaintenance/version_updates/25.Q4.00.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @noinspection PhpUnused */
+function getUpdates25_Q4_00(): array {
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+	];
+}
