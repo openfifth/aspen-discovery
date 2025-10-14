3926339b96678e33c4e3901ea0ac111f366e27f5
LeoStoyanov <leo.stoyanov24@gmail.com>
2025-10-14 18:27:20 -0500

    RMAINT: DIS-1356 - Remove OverDrive Hold Suspension Reactive Date Selection.

--- code/web/sys/Account/User.php
+++ code/web/sys/Account/User.php
@@ -2933,10 +2933,10 @@ class User extends DataObject {
 		return $result;
 	}
 
-	function freezeOverDriveHold($overDriveId, $reactivationDate): array {
+	function freezeOverDriveHold($overDriveId): array {
 		require_once ROOT_DIR . '/Drivers/OverDriveDriver.php';
 		$overDriveDriver = new OverDriveDriver();
-		return $overDriveDriver->freezeHold($this, $overDriveId, $reactivationDate);
+		return $overDriveDriver->freezeHold($this, $overDriveId);
 	}
 
 	function thawOverDriveHold($overDriveId): array {
