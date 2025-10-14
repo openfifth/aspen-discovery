fb409c3fc573ac700a036e15e9f2856fa035bee8
LeoStoyanov <leo.stoyanov24@gmail.com>
2025-10-14 18:38:39 -0500

    RMAINT: DIS-1410 - Book covers for suggestions multiple format categories have errors.

--- code/web/services/API/ListAPI.php
+++ code/web/services/API/ListAPI.php
@@ -503,7 +503,11 @@ class ListAPI extends AbstractAPI {
 							$imageUrl .= "&upc=" . $suggestion['titleInfo']['upc'];
 						}
 						if (isset($suggestion['titleInfo']['format_category'])) {
-							$imageUrl .= "&category=" . $suggestion['titleInfo']['format_category'];
+							if (is_array($suggestion['titleInfo']['format_category'])) {
+								$imageUrl .= "&category=" . reset($suggestion['titleInfo']['format_category']);
+							}else{
+								$imageUrl .= "&category=" . $suggestion['titleInfo']['format_category'];
+							}
 						}
 						$smallImageUrl = $imageUrl . "&size=small";
 						$imageUrl .= "&size=medium";
