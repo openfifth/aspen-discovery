d5c19867565e64e39ed2d3daafd06d5cc14c457e
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-10-15 15:36:23 +0100

    DIS-1191: feat: update db with grouped ids

--- code/web/sys/DBMaintenance/version_updates/26.04.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.04.00.php
@@ -218,6 +218,14 @@ function getUpdates26_04_00(): array {
 				"ALTER TABLE library ADD COLUMN allowHoldsToBeGrouped TINYINT(1) DEFAULT 0",
 			],
 		], //add_ability_for_admin_to_control_whether_holds_can_be_grouped
+		'add_grouped_hold_id_to_user_hold' => [
+			'title' => 'Add Grouped Hold Id To User Hold',
+			'description' => 'Add grouped hold id and visual hold group id to user hold',
+			'sql' => [
+				"ALTER TABLE user_hold ADD COLUMN holdGroupId INT(11) DEFAULT NULL",
+				"ALTER TABLE user_hold ADD COLUMN visualHoldGroupId VARCHAR(50) DEFAULT NULL",
+			],
+		], //add_grouped_hold_id_to_user_hold
 
 		//mark j
 		'add_pageViewsFromPlacard_to_web_builder_resource_usage' => [
