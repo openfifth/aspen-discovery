c8e25553dc4344d51f690e51bd7128075bd9f5e2
LeoStoyanov <leo.stoyanov24@gmail.com>
2025-10-17 13:57:20 -0500

    DIS-1463: Allow Themes to be soft deleted and restored through Object Restorations.

--- code/web/sys/DBMaintenance/version_updates/25.11.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.11.00.php
@@ -25,6 +25,16 @@ function getUpdates25_11_00(): array {
 		//Yanjun Li - ByWater
 
 		// Leo Stoyanov - BWS
+		'add_theme_soft_delete_columns' => [
+			'title' => 'Add Soft Delete Columns to Themes',
+			'description' => 'Add metadata needed to support Object Restorations for Themes.',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE themes ADD COLUMN deleted TINYINT(1) DEFAULT 0',
+				'ALTER TABLE themes ADD COLUMN dateDeleted INT(11) DEFAULT 0',
+				'ALTER TABLE themes ADD COLUMN deletedBy INT(11) DEFAULT NULL',
+			],
+		], //add_theme_soft_delete_columns
 
 		//alexander - Open Fifth
 
