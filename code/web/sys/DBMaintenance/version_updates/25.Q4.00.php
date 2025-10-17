155221a79339762ffcc6d0956a8436c0ba14f74d
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-10-17 12:55:05 +0100

    DIS-1458: fix: update column type for grapes

--- code/web/sys/DBMaintenance/version_updates/25.Q4.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.Q4.00.php
@@ -12,5 +12,16 @@ function getUpdates25_Q4_00(): array {
 			 ]
 		 ], //name*/
 
+		 //alexander - Open Fifth
+		 'change_data_types_for_grapes_js_columns' => [
+			'title' => 'Change Data Types For Grapes JS Columns',
+			'description' => 'Update column types to allow for longer pages',
+			'continueOnError' => false,
+			'sql' => [
+				"ALTER TABLE grapes_web_builder MODIFY templateContent LONGTEXT",
+				"ALTER TABLE grapes_web_builder MODIFY htmlData LONGTEXT",
+				"ALTER TABLE grapes_web_builder MODIFY cssData LONGTEXT",
+			]
+		], //change_data_types_for_grapes_js_columns
 	];
 }
