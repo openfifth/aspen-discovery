3b44283f694bdfeef3c0842186146cd10f8554ae
kirstien <kirstienkroeger@icloud.com>
2025-10-24 20:33:28 -0500

    Update UserListGroup.php

--- code/web/sys/UserLists/UserListGroup.php
+++ code/web/sys/UserLists/UserListGroup.php
@@ -7,6 +7,76 @@ class UserListGroup extends DataObject {
 	public $parentGroupId;
 	public $userId;
 
+	static $_objectStructure = [];
+
+	static function getObjectStructure(string $context = ''): array {
+		if (isset(self::$_objectStructure[$context]) && self::$_objectStructure[$context] !== null) {
+			return self::$_objectStructure[$context];
+		}
+
+		$structure = [
+			'id' => [
+				'property' => 'id',
+				'type' => 'label',
+				'label' => 'Id',
+				'description' => 'The unique id of the user list group.',
+				'storeDb' => true,
+				'storeSolr' => false,
+			],
+			'title' => [
+				'property' => 'title',
+				'type' => 'text',
+				'size' => 100,
+				'maxLength' => 255,
+				'label' => 'Title',
+				'description' => 'The title of the user list group.',
+				'required' => true,
+				'storeDb' => true,
+				'storeSolr' => true,
+			],
+			'parentGroupId' => [
+				'property' => 'parentGroupId',
+				'type' => 'integer',
+				'label' => 'Parent Group Id',
+				'description' => 'The id of the parent group, if any.',
+				'required' => false,
+				'storeDb' => true,
+				'storeSolr' => false,
+			],
+			'userId' => [
+				'property' => 'userId',
+				'type' => 'integer',
+				'label' => 'User Id',
+				'description' => 'The id of the user who owns this group.',
+				'required' => true,
+				'storeDb' => true,
+				'storeSolr' => false,
+			]
+		];
+
+		self::$_objectStructure[$context] = $structure;
+		return self::$_objectStructure[$context];
+	}
+
+	function numValidLists() {
+		require_once ROOT_DIR . '/sys/UserLists/UserList.php';
+		$userList = new UserList();
+		$userList->listGroupId = $this->id;
+
+		return $userList->count();
+	}
+
+	public function getUniquenessFields(): array {
+		return ['id'];
+	}
+
+	public function getNumericColumnNames(): array {
+		return [
+			'id',
+			'parentGroupId',
+			'userId'
+		];
+	}
 	public function supportsSoftDelete(): bool {
 		return true;
 	}
