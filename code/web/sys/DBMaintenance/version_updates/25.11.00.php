be614ac90b285eeedc4f340032f6e245a8a1c09c
kirstien <kirstienkroeger@icloud.com>
2025-10-24 20:02:02 -0500

    add user list groups to db

--- code/web/sys/DBMaintenance/version_updates/25.11.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.11.00.php
@@ -17,6 +17,47 @@ function getUpdates25_11_00(): array {
 		//katherine - Grove
 
 		//kirstien - Grove
+		'add_user_list_group_table' => [
+			'title' => 'Add list group table',
+			'description' => 'Add table to support grouping of lists',
+			'continueOnError' => false,
+			'sql' => [
+				'CREATE TABLE IF NOT EXISTS user_list_group (
+					id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+					title VARCHAR(255) NOT NULL,
+					parentGroupId INT(11) DEFAULT NULL,
+					userId INT(11) NOT NULL
+				)'
+			],
+		],
+		//add_user_list_group_table
+		'add_last_used_group_list_view_to_user' => [
+			'title' => 'Add last viewed list group to user',
+			'description' => 'Add column to track last viewed list group for users',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE user ADD COLUMN lastGroupListViewed INT(11) DEFAULT NULL',
+			]
+		],
+		//add_last_used_group_list_view_to_user
+		'add_last_used_group_list_added_to_user' => [
+			'title' => 'Add last added used group list to user',
+			'description' => 'Add column to track last added list group for users',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE user ADD COLUMN lastGroupListAdded INT(11) DEFAULT NULL',
+			]
+		],
+		//add_last_used_group_list_added_to_user
+		'add_group_list_id_to_user_list' => [
+			'title' => 'Add group list id to user list',
+			'description' => 'Add column to track group list id for user lists',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE user_list ADD COLUMN groupListId INT(11) DEFAULT -1',
+			]
+		],
+		//add_group_list_id_to_user_list
 
 		//kodi - Grove
 
