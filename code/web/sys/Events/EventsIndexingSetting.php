3bd3ec4fc386485eb6baace9d4117bda3b812362
Mark Noble <mdnoble73@gmail.com>
2025-11-02 13:25:57 -0700

    DIS-1448 - Event Indexing control searched libraries

--- code/web/sys/Events/EventsIndexingSetting.php
+++ code/web/sys/Events/EventsIndexingSetting.php
@@ -1,5 +1,4 @@
 <?php /** @noinspection PhpMissingFieldTypeInspection */
-require_once ROOT_DIR . '/sys/Events/LocationEventsSetting.php';
 class EventsIndexingSetting extends DataObject {
 	public $__table = 'events_indexing_settings';    // table name
 	public $id;
@@ -8,16 +7,11 @@ class EventsIndexingSetting extends DataObject {
 	/** @noinspection PhpUnused */
 	public $numberOfDaysToIndex;
 
-	private $_libraries;
-	private $_locations;
-
 	static $_objectStructure = [];
 	static function getObjectStructure(string $context = ''): array {
 		if (isset(self::$_objectStructure[$context]) && self::$_objectStructure[$context] !== null) {
 			return self::$_objectStructure[$context];
 		}
-		$libraryList = Library::getLibraryList(!UserAccount::userHasPermission('Administer Events for All Locations'));
-		$locationList = Location::getLocationList(!UserAccount::userHasPermission('Administer All Libraries') || UserAccount::userHasPermission('Administer Home Library Locations'));
 
 		$structure = [
 			'id' => [
@@ -58,168 +52,9 @@ class EventsIndexingSetting extends DataObject {
 				'label' => 'Last Update Of Changed Events',
 				'readOnly' => 1,
 			],
-			'eventsSearchSetting' => [
-				'property' => 'eventsSearchSetting',
-				'type' => 'enum',
-				'label' => 'Events Search Scope',
-				'description' => 'The search scope for events.',
-				'values' => [
-					'0' => 'All events for all libraries',
-					'1' => 'All events at any selected library',
-					'2' => 'Events for the selected library only',
-				],
-				'default' => '1',
-				'hideInLists' => true,
-			],
-			'libraries' => [
-				'property' => 'libraries',
-				'type' => 'multiSelect',
-				'listStyle' => 'checkboxSimple',
-				'label' => 'Libraries',
-				'description' => 'Define libraries that use these settings',
-				'values' => $libraryList,
-			],
-			'locations' => [
-				'property' => 'locations',
-				'type' => 'multiSelect',
-				'listStyle' => 'checkboxSimple',
-				'label' => 'Locations',
-				'description' => 'Define locations that use this type',
-				'values' => $locationList,
-			],
 		];
 
 		self::$_objectStructure[$context] = $structure;
 		return self::$_objectStructure[$context];
-	}
-
-	/**
-	 * Override the update functionality to save related objects
-	 *
-	 * @see DB/DB_DataObject::update()
-	 */
-	public function update(string $context = '') : int|bool {
-		$ret = parent::update();
-		if ($ret !== FALSE) {
-			$this->saveLibraries();
-			$this->saveLocations();
-		}
-		return $ret;
-	}
-
-	/**
-	 * Override the insert functionality to save the related objects
-	 *
-	 * @see DB/DB_DataObject::insert()
-	 */
-	public function insert(string $context = '') : int|bool {
-		$ret = parent::insert();
-		if ($ret !== FALSE) {
-			$this->saveLibraries();
-			$this->saveLocations();
-		}
-		return $ret;
-	}
-
-	public function __get($name) {
-		if ($name == "libraries") {
-			return $this->getLibraries();
-		} else if ($name == "locations") {
-			return $this->getLocations();
-		} else {
-			return parent::__get($name);
-		}
-	}
-
-	public function __set($name, $value) {
-		if ($name == "libraries") {
-			$this->_libraries = $value;
-		} else if ($name == "locations") {
-			$this->_locations = $value;
-		} else {
-			parent::__set($name, $value);
-		}
-	}
-
-	public function delete(bool $useWhere = false, bool $hardDelete = false) : bool|int {
-		$ret = parent::delete($useWhere, $hardDelete);
-		if ($ret && !empty($this->id)) {
-			$this->clearLibraries();
-			$this->clearLocations();
-		}
-		return $ret;
-	}
-
-	public function getLibraries() : ?array {
-		if (!isset($this->_libraries) && $this->id) {
-			$this->_libraries = [];
-			$library = new LibraryEventsSetting();
-			$library->settingSource = 'aspenEvents';
-			$library->settingId = $this->id;
-			$library->find();
-			while ($library->fetch()) {
-				$this->_libraries[$library->libraryId] = $library->libraryId;
-			}
-		}
-		return $this->_libraries;
-	}
-
-	public function saveLibraries() : void {
-		if (isset($this->_libraries) && is_array($this->_libraries)) {
-			$this->clearLibraries();
-
-			foreach ($this->_libraries as $libraryId) {
-				$libraryEventSetting = new LibraryEventsSetting();
-
-				$libraryEventSetting->settingSource = 'aspenEvents';
-				$libraryEventSetting->settingId = $this->id;
-				$libraryEventSetting->libraryId = $libraryId;
-				$libraryEventSetting->insert();
-			}
-			unset($this->_libraries);
-		}
-	}
-
-	private function clearLibraries() : void {
-		//Delete links to the libraries
-		$libraryEventSetting = new LibraryEventsSetting();
-		$libraryEventSetting->settingSource = 'aspenEvents';
-		$libraryEventSetting->settingId = $this->id;
-		$libraryEventSetting->delete(true);
-	}
-
-	public function getLocations() : ?array {
-		if (!isset($this->_locations) && $this->id) {
-			$this->_locations = [];
-			$location = new LocationEventsSetting();
-			$location->settingId = $this->id;
-			$location->find();
-			while ($location->fetch()) {
-				$this->_locations[$location->locationId] = $location->locationId;
-			}
-		}
-		return $this->_locations;
-	}
-
-	public function saveLocations() : void {
-		if (isset($this->_locations) && is_array($this->_locations)) {
-			$this->clearLocations();
-
-			foreach ($this->_locations as $locationId) {
-				$locationEventsSetting = new LocationEventsSetting();
-
-				$locationEventsSetting->settingId = $this->id;
-				$locationEventsSetting->locationId = $locationId;
-				$locationEventsSetting->insert();
-			}
-			unset($this->_locations);
-		}
-	}
-
-	private function clearLocations() : void {
-		//Delete links to the libraries
-		$libraryEventSetting = new LocationEventsSetting();
-		$libraryEventSetting->settingId = $this->id;
-		$libraryEventSetting->delete(true);
 	}
 }
