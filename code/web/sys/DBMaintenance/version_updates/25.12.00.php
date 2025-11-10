c378ee110d89f220469e17edd4788e792cf2db40
LeoStoyanov <leo.stoyanov24@gmail.com>
2025-11-10 11:09:21 -0600

    DIS-1571: Add DB update to copy "location" Translation Map values into the Location table facet labels.

--- code/web/sys/DBMaintenance/version_updates/25.12.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.12.00.php
@@ -23,6 +23,18 @@ function getUpdates25_12_00(): array {
 		//Yanjun Li - ByWater
 
 		// Leo Stoyanov - BWS
+		'populate_location_facet_labels' => [
+			'title' => 'Populate Location Facet Labels',
+			'description' => 'Copy legacy "location" Translation Map values into the Location table facet labels based on matching codes.',
+			'continueOnError' => false,
+			'sql' => [
+				"UPDATE location
+					INNER JOIN translation_maps tm ON tm.name = 'location'
+					INNER JOIN translation_map_values tmv ON tmv.translationMapId = tm.id
+					SET location.facetLabel = tmv.translation
+					WHERE LOWER(location.code) = LOWER(tmv.value)"
+			]
+		], //populate_location_facet_labels
 
 		//alexander - Open Fifth
 
