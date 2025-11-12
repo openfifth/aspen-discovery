c80be93b64b13f49345930f26563c1773d9ea310
LeoStoyanov <leo.stoyanov24@gmail.com>
2025-11-11 20:19:32 -0600

    DIS-1581: Fix spacing of property and move DB update.

--- code/web/sys/DBMaintenance/version_updates/25.12.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.12.00.php
@@ -23,6 +23,32 @@ function getUpdates25_12_00(): array {
 		//Yanjun Li - ByWater
 
 		// Leo Stoyanov - BWS
+		'library_self_reg_form_message_translations' => [
+			'title' => 'Library - Translate Self Registration Form Message',
+			'description' => 'Copy existing self registration form messages into the text block translation table for all available languages.',
+			'continueOnError' => false,
+			'sql' => [
+				"INSERT INTO text_block_translation (objectType, objectId, fieldName, languageId, translation)
+				SELECT 'Library', l.libraryId, 'selfRegistrationFormMessage', lang.id, l.selfRegistrationFormMessage
+				FROM library l
+				JOIN languages lang ON 1=1
+				LEFT JOIN text_block_translation existing ON existing.objectType = 'Library' AND existing.objectId = l.libraryId AND existing.fieldName = 'selfRegistrationFormMessage' AND existing.languageId = lang.id
+				WHERE l.selfRegistrationFormMessage IS NOT NULL AND l.selfRegistrationFormMessage <> '' AND existing.id IS NULL AND lang.code NOT IN ('ubb','pig')"
+			]
+		], //library_self_reg_form_message_translations
+		'library_self_reg_success_message_translations' => [
+			'title' => 'Library - Translate Self Registration Success Message',
+			'description' => 'Copy existing self registration success messages into the text block translation table for all available languages.',
+			'continueOnError' => false,
+			'sql' => [
+				"INSERT INTO text_block_translation (objectType, objectId, fieldName, languageId, translation)
+				SELECT 'Library', l.libraryId, 'selfRegistrationSuccessMessage', lang.id, l.selfRegistrationSuccessMessage
+				FROM library l
+				JOIN languages lang ON 1=1
+				LEFT JOIN text_block_translation existing ON existing.objectType = 'Library' AND existing.objectId = l.libraryId AND existing.fieldName = 'selfRegistrationSuccessMessage' AND existing.languageId = lang.id
+				WHERE l.selfRegistrationSuccessMessage IS NOT NULL AND l.selfRegistrationSuccessMessage <> '' AND existing.id IS NULL AND lang.code NOT IN ('ubb','pig')"
+			]
+		], //library_self_reg_success_message_translations
 
 		//alexander - Open Fifth
 
