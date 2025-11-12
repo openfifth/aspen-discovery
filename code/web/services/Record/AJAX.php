c117b7d8572b7a3cda1c542547b88f945d4436a9
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-11-12 13:28:23 +0000

    DIS-1345: refactor: swap to API requests

--- code/web/services/Record/AJAX.php
+++ code/web/services/Record/AJAX.php
@@ -1180,7 +1180,8 @@ class Record_AJAX extends Action {
 						/** @var Koha $catalogDriver */
 						$catalogDriver = $user->getCatalogDriver();
 						if ($catalogDriver->hasHoldFeeMessage()) {
-							$reserveFeeMessage = $catalogDriver->getPostHoldSubmissionFeeMessage();
+							$marcRecord = RecordDriverFactory::initRecordDriverById($recordId);
+							$reserveFeeMessage = $catalogDriver->getPostHoldSubmissionFeeMessage($marcRecord);
 							if ($reserveFeeMessage) {
 								$interface->assign('reserveFeeMessage', $reserveFeeMessage);
 							}
@@ -1985,7 +1986,7 @@ class Record_AJAX extends Action {
 		/** @var Koha $catalogDriver */
 		$catalogDriver = $marcRecord->getCatalogDriver();
 		if ($catalogDriver->hasHoldFeeMessage()) {
-			$reserveFeeMessage = $catalogDriver->getPreHoldSubmissionFeeMessage();
+			$reserveFeeMessage = $catalogDriver->getPreHoldSubmissionFeeMessage($marcRecord);
 			if ($reserveFeeMessage) {
 				$interface->assign('reserveFeeMessage', $reserveFeeMessage);
 			}
