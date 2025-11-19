fc33077533ae46df8e6d274ce9e7a74a8a3f89ec
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-11-19 18:02:33 +0000

    DIS-1597: feat: register logged in user to event
    Handles registration attempts
    Display registration outcome to the user as either
    succesfull or failed (if event instance not
    found, user not found (unlikely), or user
    already registered)

---
+++ code/web/sys/Events/UserAspenEventInstanceRegistration.php
@@ -0,0 +1,15 @@
+<?php /** @noinspection PhpMissingFieldTypeInspection */
+require_once ROOT_DIR . '/sys/Events/EventInstance.php';
+
+class UserAspenEventInstanceRegistration extends DataObject {
+	public $__table = 'user_aspen_event_instance_registrations';
+	public $id;
+	public $userId;
+	public $eventInstanceId;
+	public $success;
+	public $attended;
+
+	public function isUserRegisteredForEvent(): bool {
+		return $this->find(true);
+	}
+}
