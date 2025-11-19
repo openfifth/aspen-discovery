815a423e32e0cd5023ee5dc97e1659332c385b9a
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-11-19 12:56:13 +0000

    DIS-1597: chore: remove link to libraries
    Settings that affect indexing should not be linked
    to specific libraries as native aspen event
    indexing is system-level.

--- code/web/sys/Events/AspenEventSetting.php
+++ code/web/sys/Events/AspenEventSetting.php
@@ -14,17 +14,11 @@ class AspenEventSetting extends DataObject {
 	/** @noinspection PhpUnused */
 	public $numberOfDaysToIndex;
 
-	private $_libraries;
-	private $_locationMap;
-
-
 	static $_objectStructure = [];
 	static function getObjectStructure(string $context = ''): array {
 		if (isset(self::$_objectStructure[$context]) && self::$_objectStructure[$context] !== null) {
 			return self::$_objectStructure[$context];
 		}
-		$libraryList = Library::getLibraryList(!UserAccount::userHasPermission('Administer Events for All Locations'));
-
 		/** @noinspection HtmlRequiredAltAttribute */
 		$structure = [
 			'id' => [
@@ -91,15 +85,6 @@ class AspenEventSetting extends DataObject {
 					],
 				]
 			],
-			'libraries' => [
-				'property' => 'libraries',
-				'type' => 'multiSelect',
-				'listStyle' => 'checkboxSimple',
-				'label' => 'Libraries',
-				'description' => 'Define libraries that use these settings',
-				'values' => $libraryList,
-				'hideInLists' => true,
-			],
 		];
 
 		self::$_objectStructure[$context] = $structure;
@@ -112,124 +97,5 @@ class AspenEventSetting extends DataObject {
 			return $interface->fetch('AspenEvents/registrationModalBody.tpl');
 		} 
 		return $this->registrationModalBody;
-	}
-
-	/**
-	 * Override the update functionality to save related objects
-	 *
-	 * @see DB/DB_DataObject::update()
-	 */
-	public function update(string $context = '') : int|bool {
-		$ret = parent::update();
-		if ($ret !== FALSE) {
-			$this->saveLibraries();
-		}
-		return $ret;
-	}
-
-	/**
-	 * Override the insert functionality to save the related objects
-	 *
-	 * @see DB/DB_DataObject::insert()
-	 */
-	public function insert(string $context = '') : int|bool {
-		$ret = parent::insert();
-		if ($ret !== FALSE) {
-			$this->saveLibraries();
-		}
-		return $ret;
-	}
-
-	public function __get($name) {
-		if ($name == "libraries") {
-			return $this->getLibraries();
-		}
-		if ($name == "locationMap") {
-			return $this->getLocationMap();
-		} else {
-			return parent::__get($name);
-		}
-	}
-
-	public function __set($name, $value) {
-		if ($name == "libraries") {
-			$this->_libraries = $value;
-		} else {
-			parent::__set($name, $value);
-		}
-	}
-
-	public function delete(bool $useWhere = false, bool $hardDelete = false) : bool|int {
-		$ret = parent::delete($useWhere, $hardDelete);
-		if ($ret && !empty($this->id)) {
-			$this->clearLibraries();
-		}
-		return $ret;
-	}
-
-	public function getLibraries() : ?array {
-		if (!isset($this->_libraries) && $this->id) {
-			$this->_libraries = [];
-			$library = new LibraryEventsSetting();
-			$library->settingSource = 'aspenEvent';
-			$library->settingId = $this->id;
-			$library->find();
-			while ($library->fetch()) {
-				$this->_libraries[$library->libraryId] = $library->libraryId;
-			}
-		}
-		return $this->_libraries;
-	}
-
-	public function getLocationMap() : array {
-		if (!isset($this->_locationMap)) {
-			//Get the list of translation maps
-			$this->_locationMap = [];
-			$locationMap = new EventsBranchMapping();
-			$locationMap->orderBy('id');
-			$locationMap->find();
-			while ($locationMap->fetch()) {
-				$this->_locationMap[$locationMap->id] = clone($locationMap);
-			}
-		}
-		return $this->_locationMap;
-	}
-
-	public function saveLibraries() : void {
-		if (isset($this->_libraries) && is_array($this->_libraries)) {
-			$this->clearLibraries();
-
-			foreach ($this->_libraries as $libraryId) {
-				$libraryEventSetting = new LibraryEventsSetting();
-
-				$libraryEventSetting->settingSource = 'aspenEvent';
-				$libraryEventSetting->settingId = $this->id;
-				$libraryEventSetting->libraryId = $libraryId;
-				$libraryEventSetting->insert();
-			}
-			unset($this->_libraries);
-		}
-	}
-
-	public function saveLocationMap() : void {
-		if (isset($this->_locationMap)) {
-			foreach ($this->_locationMap as $location) {
-				$locationMap = new EventsBranchMapping();
-				$locationMap->locationId = $location->locationId;
-				if ($locationMap->find(true)) {
-					$locationMap->eventsLocation = $location->eventsLocation;
-					$locationMap->update();
-				}
-			}
-			unset($this->_locationMap);
-		}
-	}
-
-	private function clearLibraries() : void {
-		//Delete links to the libraries
-		$libraryEventSetting = new LibraryEventsSetting();
-		$libraryEventSetting->settingSource = 'aspenEvent';
-		$libraryEventSetting->settingId = $this->id;
-		$libraryEventSetting->delete(true);
 	}
 }
