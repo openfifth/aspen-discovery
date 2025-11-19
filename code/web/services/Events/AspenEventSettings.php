c483941a1ddfa519f93ba80a222485e55ef14c89
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-11-19 11:03:40 +0000

    DIS-1597: docs: remove redundant comments

--- code/web/services/Events/AspenEventSettings.php
+++ code/web/services/Events/AspenEventSettings.php
@@ -5,17 +5,10 @@ require_once ROOT_DIR . '/services/Admin/ObjectEditor.php';
 require_once ROOT_DIR . '/sys/Events/AspenEventSetting.php';
 
 class Events_AspenEventSettings extends ObjectEditor {
-
-	/**
-	 * The class name of the object which is being edited
-	 */
 	function getObjectType(): string {
 		return 'AspenEventSetting';
 	}
 
-	/**
-	 * The page name of the tool (typically the plural of the object)
-	 */
 	function getToolName(): string {
 		return 'AspenEventSettings';
 	}
@@ -24,16 +17,10 @@ class Events_AspenEventSettings extends ObjectEditor {
 		return 'Events';
 	}
 
-	/**
-	 * The title of the page to be displayed
-	 */
 	function getPageTitle(): string {
 		return 'Aspen Event Settings';
 	}
 
-	/**
-	 * Load all objects into an array keyed by the primary key
-	 */
 	function getAllObjects($page, $recordsPerPage): array {
 		$object = new AspenEventSetting();
 		$object->limit(($page - 1) * $recordsPerPage, $recordsPerPage);
@@ -51,24 +38,14 @@ class Events_AspenEventSettings extends ObjectEditor {
 		return 'id asc';
 	}
 
-	/**
-	 * Define the properties which are editable for the object
-	 * as well as how they should be treated while editing, and a description for the property
-	 */
 	function getObjectStructure($context = ''): array {
 		return AspenEventSetting::getObjectStructure($context);
 	}
 
-	/**
-	 * The name of the column which defines this as unique
-	 */
 	function getPrimaryKeyColumn(): string {
 		return 'id';
 	}
 
-	/**
-	 * The id of the column which serves to join other columns
-	 */
 	function getIdKeyColumn(): string {
 		return 'id';
 	}
