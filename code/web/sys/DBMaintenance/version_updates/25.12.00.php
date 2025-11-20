b3914e021554f231559f89d804d5147847515954
LeoStoyanov <leo.stoyanov24@gmail.com>
2025-11-20 16:39:00 -0600

    DIS-1616: Add DB update.

--- code/web/sys/DBMaintenance/version_updates/25.12.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.12.00.php
@@ -23,6 +23,14 @@ function getUpdates25_12_00(): array {
 		//Yanjun Li - ByWater
 
 		// Leo Stoyanov - BWS
+		'grouped_work_display_settings_numSeriesToShowBeforeMore' => [
+			'title' => 'Grouped Work Display Settings - Add Number of Series to Show Before "More Series" Link',
+			'description' => 'Add numSeriesToShowBeforeMore field to grouped_work_display_settings table to configure the number of series entries displayed before showing "More Series..." link.',
+			'continueOnError' => false,
+			'sql' => [
+				"ALTER TABLE grouped_work_display_settings ADD COLUMN IF NOT EXISTS numSeriesToShowBeforeMore INT(11) DEFAULT 3",
+			]
+		],
 
 		//alexander - Open Fifth
 
