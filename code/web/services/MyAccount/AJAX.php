5143fbae1c6bb5cdcf00731761f6f5d28d43e1de
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-11-25 12:18:22 +0000

    DIS-1597: feat: register to saved event

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -4690,6 +4690,7 @@ class MyAccount_AJAX extends JSON_Action {
 		$pager = new Pager($options);
 		$interface->assign('pageLinks', $pager->getLinks());
 		$interface->assign('events', $events);
+		$interface->assign('userId', $user->id);
 
 		$result['success'] = true;
 		$result['message'] = "";
