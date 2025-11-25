99677c45ff37c7df5165e1b2af759bf0e91e6199
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-11-25 13:00:37 +0000

    DIS-1597: feat: check user registration

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -4648,7 +4648,7 @@ class MyAccount_AJAX extends JSON_Action {
 				require_once ROOT_DIR . '/sys/Events/UserAspenEventInstanceRegistration.php';
 				$aspenEventRegistration = new UserAspenEventInstanceRegistration();
 				$aspenEventRegistration->userdId = UserAccount::getActiveUserId();
-				$aspenEventRegistration->eventInstanceId = $eventry->sourceId;
+				$aspenEventRegistration->eventInstanceId = $entry->sourceId;
 				$registration = $aspenEventRegistration->isUserRegisteredForEvent();
 			}
 
