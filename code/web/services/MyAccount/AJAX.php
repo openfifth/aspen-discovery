226f1ee09bac1bc4384b0048e354c7bf1e2523e2
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-11-25 13:00:37 +0000

    DIS-1597: feat: check user registration
    (cherry picked from commit dca9d7c6e09200b670d9fef42ad17fc37a72e098)
    Signed-off-by: Chloé Zermatten <chloe.zermatten@openfifth.co.uk>

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -4129,6 +4129,16 @@ class MyAccount_AJAX extends JSON_Action {
 
 		foreach ($eventIds as $curEventId => $entry) {
 			$registration = UserAccount::getActiveUserObj()->isRegistered($entry->sourceId);
+			
+			// check apen native events registration
+			if(!strpos('aspenEvent',$entry->sourceId)) {
+				require_once ROOT_DIR . '/sys/Events/UserAspenEventInstanceRegistration.php';
+				$aspenEventRegistration = new UserAspenEventInstanceRegistration();
+				$aspenEventRegistration->userdId = UserAccount::getActiveUserId();
+				$aspenEventRegistration->eventInstanceId = $eventry->sourceId;
+				$registration = $aspenEventRegistration->isUserRegisteredForEvent();
+			}
+
 			if (array_key_exists($curEventId, $eventRecords)) {
 				$eventRecordDriver = $eventRecords[$curEventId];
 				$events[$entry->sourceId] = [
