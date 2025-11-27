80f71589489160e462b8984a8a86a00a55241009
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-11-27 12:58:49 +0000

    DIS-1597: chore: mergejs

--- code/web/interface/themes/responsive/js/aspen.js
+++ code/web/interface/themes/responsive/js/aspen.js
@@ -4461,7 +4461,7 @@ AspenDiscovery.Account = (function () {
 			return false;
 		},
 
-		registerUserToEvent: function (eventSourceId) {
+		toggleUserEventRegistration: function (eventSourceId) {
 			if (!Globals.loggedIn) {
 				return;
 			}
@@ -4472,9 +4472,9 @@ AspenDiscovery.Account = (function () {
 
 			const url = Globals.path + "/MyAccount/AJAX";
 			const params = {
-				method: 'registerUserToEvent',
+				method: 'toggleUserRegistrationToEvent',
 				eventInstanceId: eventSourceId.replace(/aspenEvent_\d+_/, ''),
-				userId: userId
+				userId
 			};
 
 			$.getJSON(url, params, function (data) {
