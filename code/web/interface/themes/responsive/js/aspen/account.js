47c438eee60cc999c4446ae5f0e699d77c7afb38
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-11-27 19:46:55 +0000

    DIS-1597: feat: responsive registration toggle

--- code/web/interface/themes/responsive/js/aspen/account.js
+++ code/web/interface/themes/responsive/js/aspen/account.js
@@ -2301,6 +2301,10 @@ AspenDiscovery.Account = (function () {
 			const email = selectedOption.dataset.email || '';
 			const location = selectedOption.dataset.location || '';
 
+			const userIdField = document.getElementById(`eventRegistrationUserId-${eventSourceId}`);
+			const userId = selector ? selector.value : (userIdField ? userIdField.value : null);
+
+			AspenDiscovery.Account.isUserRegisteredForEvent(eventSourceId, userId).then((data) => AspenDiscovery.Account.updateEventButtonTextContent(data, eventSourceId));
 			document.getElementById(`eventRegistrationUserId-${eventSourceId}`).value = selector.value;
 			document.getElementById(`eventUserEmail-${eventSourceId}`).textContent = email;
 			document.getElementById(`eventUserLocation-${eventSourceId}`).textContent = location;
@@ -2310,6 +2314,16 @@ AspenDiscovery.Account = (function () {
 				const primaryUserId = selector.options[0].value;
 				changeLink.style.display = (selector.value === primaryUserId) ? '' : 'none';
 			}
+		},
+
+		updateEventButtonTextContent: function (userRegistrationData, eventSourceId) {
+			if (!userRegistrationData.success) {
+				document.getElementById(`aspen-events-toggle-registration-button-${eventSourceId}`).textContent = 'Unavailable';
+				document.getElementById(`aspen-events-toggle-registration-button-${eventSourceId}`).setAttribute('disabled', true);
+				return;
+			}
+			document.getElementById(`aspen-events-toggle-registration-button-${eventSourceId}`).textContent = userRegistrationData.body.isRegistered ? 'Unregister' : 'Register';
+			document.getElementById(`aspen-events-toggle-registration-button-${eventSourceId}`).removeAttribute('disabled');
 		},
 
 		isUserRegisteredForEvent: function (eventSourceId, userId) {
