dc6e5f7444ce6376f60500afba2e07a64da4f848
Kyle M Hall <kyle@bywatersolutions.com>
2025-12-09 14:55:32 -0500

    RMAINT: Update version to 26.Q1.00 EA

---
+++ code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @noinspection PhpUnused */
+function getUpdates26_Q1_00(): array {
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+	];
+}
