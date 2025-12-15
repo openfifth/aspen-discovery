245fe0b667a1737d4416598c370c8cd838fa46e0
Kodi <kodilein@gmail.com>
2025-12-15 14:16:12 -0800

    DIS-1705: Add ability to customize events calendar title
    Add setting in Calendar Display Settings for Calendar Title
    Update DB for new setting
    Update release notes
    
    Tested on my local instance of Aspen

--- code/web/sys/DBMaintenance/version_updates/26.01.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.01.00.php
@@ -17,6 +17,13 @@ function getUpdates26_01_00(): array {
 		//kirstien
 
 		//kodi
+		'events_calendar_title' => [
+			'title' => 'Events Calendar Title',
+			'description' => 'Add the ability to create titles for event calendars in Calendar Display Settings',
+			'sql' => [
+				"ALTER TABLE calendar_display_settings ADD COLUMN calendarTitle VARCHAR(255) DEFAULT 'Events Calendar'",
+			]
+		], //events_calendar_title
 
 		//leo
 
