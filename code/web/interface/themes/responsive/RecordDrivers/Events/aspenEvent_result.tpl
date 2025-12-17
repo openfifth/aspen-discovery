1b07f8a71cb2bc44208ba52acb371dade2d2a4a8
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2025-12-17 17:30:56 +0000

    DIS-1715: feat: add waiting list seats to ui

--- code/web/interface/themes/responsive/RecordDrivers/Events/aspenEvent_result.tpl
+++ code/web/interface/themes/responsive/RecordDrivers/Events/aspenEvent_result.tpl
@@ -67,6 +67,14 @@
 							{/if}
 						</div>
 					{/if}
+					{if $waitingList == true}
+						<br />
+						<div class="clearfix"></div>
+						<div class="result-label col-tn-2">{translate text="Available Seats on Waiting List" isPublicFacing=true} </div>
+						<div class="result-value col-tn-6 notranslate">
+							{$waitingListNumberOfSeats}
+						</div>
+					{/if}
 					{if $private}
 						<div class="result-value col-tn-8">
 							<span class="label label-default">{translate text="Private" isPublicFacing=true}</span>
--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -4299,6 +4299,8 @@ class MyAccount_AJAX extends JSON_Action {
 				if ($eventInstance->find(true)) {
 					$numberOfSeats = $eventInstance->getEffectiveNumberOfSeats();
 					$availableSeats = $eventInstance->getAvailableSeats();
+					$waitingList = $eventInstance->waitingList;
+					$waitingListNumberOfSeats = $eventInstance->waitingListNumberOfSeats;
 				}
 			}
 
@@ -4337,6 +4339,8 @@ class MyAccount_AJAX extends JSON_Action {
 				$events[$entry->sourceId]['numberOfSeats'] = $numberOfSeats;
 				$events[$entry->sourceId]['availableSeats'] = $availableSeats;
 				$events[$entry->sourceId]['isEventFull'] = !$eventInstance->hasAvailableSeats();
+				$events[$entry->sourceId]['waitingList'] = $waitingList;
+				$events[$entry->sourceId]['waitingListNumberOfSeats'] = $waitingListNumberOfSeats;
 			}
 		}
 
