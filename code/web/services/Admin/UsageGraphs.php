b227234c17675844254f24c14b95c348f940f3c1
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-12-23 14:54:53 +0000

    DIS-1689: feat: view aspen daily usage

--- code/web/services/Admin/UsageGraphs.php
+++ code/web/services/Admin/UsageGraphs.php
@@ -29,16 +29,20 @@ class Admin_UsageGraphs extends Admin_AbstractUsageGraphs {
 
 		$dataSeries = [];
 		$columnLabels = [];
+
+		$groupByTimeframe = implode(',', $timeframes);
+
 		// Default: AspenUsage
 		$userUsage = new AspenUsage();
-		$userUsage->groupBy('year, month');
+		$userUsage->groupBy($groupByTimeframe);
 		if (!empty($instanceName)) {
 			$userUsage->instance = $instanceName;
 		}
 		$userUsage->selectAdd();
-		$userUsage->selectAdd('year');
-		$userUsage->selectAdd('month');
-		$userUsage->orderBy('year, month');
+		foreach ($timeframes as $timeframe) {
+			$userUsage->selectAdd($timeframe);
+		}
+		$userUsage->orderBy($groupByTimeframe);
 
 		//General Usage Stats
 		if ($stat == 'pageViews' || $stat == 'generalUsage') {
