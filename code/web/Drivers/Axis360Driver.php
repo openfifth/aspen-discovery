7a44b174ab904d520ca6bf5a8cd3c808d233b531
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2025-12-23 13:14:10 +0000

    DIS-1689: feat: Axis 360 daily record and user usage

--- code/web/Drivers/Axis360Driver.php
+++ code/web/Drivers/Axis360Driver.php
@@ -702,6 +702,7 @@ class Axis360Driver extends AbstractEContentDriver {
 		$userUsage->userId = $user->id;
 		$userUsage->year = date('Y');
 		$userUsage->month = date('n');
+		$userUsage->day = date('d');
 		global $aspenUsage;
 		global $library;
 		$userUsage->instance = $aspenUsage->getInstance();
@@ -733,6 +734,7 @@ class Axis360Driver extends AbstractEContentDriver {
 			$recordUsage->instance = $aspenUsage->getInstance();
 			$recordUsage->year = date('Y');
 			$recordUsage->month = date('n');
+			$recordUsage->day = date('d');
 			if ($recordUsage->find(true)) {
 				$recordUsage->timesCheckedOut++;
 				$recordUsage->update();
@@ -759,6 +761,7 @@ class Axis360Driver extends AbstractEContentDriver {
 			$recordUsage->axis360Id = $product->axis360Id;
 			$recordUsage->year = date('Y');
 			$recordUsage->month = date('n');
+			$recordUsage->day = date('d');
 			if ($recordUsage->find(true)) {
 				$recordUsage->timesHeld++;
 				$recordUsage->update();
