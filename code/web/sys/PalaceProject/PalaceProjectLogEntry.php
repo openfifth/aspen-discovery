a0fbf9456fd0a3973c47490f090369ad53761caa
Mark Noble <mdnoble73@gmail.com>
2025-12-31 13:21:00 -0700

    DIS-1758 Improve performance of the Palace Project Indexer

--- code/web/sys/PalaceProject/PalaceProjectLogEntry.php
+++ code/web/sys/PalaceProject/PalaceProjectLogEntry.php
@@ -5,6 +5,7 @@ require_once ROOT_DIR . '/sys/BaseLogEntry.php';
 class PalaceProjectLogEntry extends BaseLogEntry {
 	public $__table = 'palace_project_export_log';   // table name
 	public $id;
+	public $settingId;
 	public $notes;
 	public $numProducts;
 	public $numErrors;
