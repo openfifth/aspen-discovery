aef97a95d8153cc1356647ba31fece5eaa3450b8
Yanjun Li <74245023+LiYanjun19@users.noreply.github.com>
2026-01-05 21:23:59 -0500

    DIS-1792: Display Hoopla record price for both version in staff view when available
    Signed-off-by: AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>

--- code/web/RecordDrivers/HooplaRecordDriver.php
+++ code/web/RecordDrivers/HooplaRecordDriver.php
@@ -105,7 +105,14 @@ class HooplaRecordDriver extends GroupedWorkSubDriver {
 
 		$rawData = $this->hooplaRawMetadata;
 		if (IPAddress::showDebuggingInformation()) {
-			$interface->assign('price', $rawData->price ?? 0);
+			if (isset($this->hooplaExtract->ppuPrice)) {
+				$price = $this->hooplaExtract->ppuPrice;
+			} elseif (isset($this->hooplaExtract->price)) {
+				$price = $this->hooplaExtract->price;
+			} else {
+				$price = 0;
+			}
+			$interface->assign('price', $price);
 		}
 		unset($rawData->price);
 
