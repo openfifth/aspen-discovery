3eda86d3f17dab6c07cdb8614d342fbe9c683357
Yanjun Li <74245023+LiYanjun19@users.noreply.github.com>
2026-01-06 15:27:02 -0500

    DIS-1714: Add DB update, releast notes, rebuild jar for 26.Q1.00

--- code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
@@ -3,13 +3,14 @@
 /** @noinspection PhpUnused */
 function getUpdates26_Q1_00(): array {
 	return [
-		/*'name' => [
-			 'title' => '',
-			 'description' => '',
-			 'continueOnError' => false,
-			 'sql' => [
-				 ''
-			 ]
-		 ], //name*/
+		//yanjun
+		'overdrive_suppress_kindle_format' => [
+			'title' => 'OverDrive Suppress Kindle Format',
+			'description' => 'Allow OverDrive scopes to suppress Kindle format in the grouped work',
+			'continueOnError' => false,
+			'sql' => [
+				"ALTER TABLE overdrive_scopes ADD COLUMN suppressKindleFormat TINYINT(1) DEFAULT 0",
+			]
+		],
 	];
 }
