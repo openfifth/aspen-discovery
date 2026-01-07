a3d4910049e80ea824fcb3ab493023eb83f9c514
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2026-01-07 15:18:28 +0000

    DIS-1715: fix: fix typo
    (cherry picked from commit 27089d966abde2f7eeb23b068c7cafb0288d2661)
    Signed-off-by: Chloé Zermatten <chloe.zermatten@openfifth.co.uk>

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -4150,7 +4150,7 @@ class MyAccount_AJAX extends JSON_Action {
 				require_once ROOT_DIR . '/sys/Events/UserAspenEventInstanceWaitingList.php';
 				$userWaitingList = new UserAspenEventInstanceWaitingList();
 				$userWaitingList->eventInstanceId = preg_replace("/aspenEvent_\d+_/", '', $entry->sourceId);
-				$userWaitingList->userId = UserAccount::getActiveuserId();
+				$userWaitingList->userId = UserAccount::getActiveUserId();
 				$userWaitingList->whereAdd('status IN ("waiting", "notified")');
 				$userOnWaitingList = $userWaitingList->find(true);
 				$userWaitingListPosition = null;
