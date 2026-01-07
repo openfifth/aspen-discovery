38271f7193fe1c5d28e32c18aa62cec62fbfa50d
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2026-01-07 11:02:22 +0000

    DIS-1715: feat: Add email template
    Add email template and functionality for sending notifications to sign
    up to events from waiting list

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -12370,6 +12370,7 @@ class MyAccount_AJAX extends JSON_Action {
 		require_once ROOT_DIR . '/sys/Email/Mailer.php';
 		require_once ROOT_DIR . '/sys/Account/User.php';
 		require_once ROOT_DIR . '/sys/Events/EventInstance.php';
+		require_once ROOT_DIR . '/sys/Events/Event.php';
 
 		$user = new User();
 		$user->id = $userId;
@@ -12383,6 +12384,27 @@ class MyAccount_AJAX extends JSON_Action {
 			return false;
 		}
 
+		$event = new Event();
+		$event->id = $eventInstance->eventId;
+		if (!$event->find(true)) {
+			return false;
+		}
+
+		$emailTemplate = EmailTemplate::getActiveTemplate('registerForEventFromWaitingList');
+		if (!$emailTemplate) {
+			global $logger;
+			$logger->log("Unable to find email template", Logger::LOG_ERROR);
+			return false;
+		}
+
+		$parameters = [
+			'eventTitle' => $event->title,
+			'eventDate' => $eventInstance->date,
+			'eventTime' => $eventInstance->time,
+			'user' => $user,
+		];
+		
+		$emailTemplate->sendEmail($user->email, $parameters);
 		return true;
 	}
  }
