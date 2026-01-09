a232d93cba89f5abe14d60371018bde2efa2d144
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2026-01-09 16:00:19 +0000

    DIS-1715: feat: events registration account notifications

--- code/web/services/MyAccount/Home.php
+++ code/web/services/MyAccount/Home.php
@@ -14,6 +14,7 @@ class MyAccount_Home extends MyAccount {
 			$homeLibrary = $user->getHomeLibrary();
 			$homeLibraryCommunityEngagementHighlight = $homeLibrary ? $homeLibrary->highlightCommunityEngagement : null;
 			$homeLibraryHighlightOpenToEnrollCampaigns = $homeLibrary ? $homeLibrary->highlightCommunityEngagementOpenToEnroll : null;
+			$homeLibraryDisplayEventNotifications = $homeLibrary ? $homeLibrary->displayEventNotificationsInAccount :  null;
 			$campaignsToEnroll = [];
 
 			if ($homeLibraryHighlightOpenToEnrollCampaigns) {
@@ -22,11 +23,15 @@ class MyAccount_Home extends MyAccount {
 				$userId  = $user->id;
 				$campaignsToEnroll = Campaign::filterByCanEnroll($allCampaigns, $userId);
 			}
+
+			$userHasEventsToRegister = $this->checkForCampaignsNowOpenToRegistration($user);
 			// Check to see if the user has rated any titles
 			$interface->assign('hasRatings', $user->hasRatings());
 			$interface->assign('highlightCommunityEngagement', $homeLibraryCommunityEngagementHighlight);
 			$interface->assign('highlightOpenToEnrollCampaigns', $homeLibraryHighlightOpenToEnrollCampaigns);
 			$interface->assign('campaignsToEnroll', $campaignsToEnroll);
+			$interface->assign('displayEventNotificationsInAccount', $homeLibraryDisplayEventNotifications);
+			$interface->assign('userHasEventsToRegister', $userHasEventsToRegister);
 
 			parent::display('home.tpl');
 		}
@@ -36,5 +41,18 @@ class MyAccount_Home extends MyAccount {
 		$breadcrumbs = [];
 		$breadcrumbs[] = new Breadcrumb('', 'Your Account');
 		return $breadcrumbs;
+	}
+
+	private function checkForCampaignsNowOpenToRegistration($user): bool {
+		require_once ROOT_DIR . '/sys/Events/UserAspenEventInstanceWaitingList.php';
+
+		$waitingList = new UserAspenEventInstanceWaitingList();
+		$waitingList->userId = $user;
+		$waitingList->canRegister = 1;
+
+		$now = date('Y-m-d H:i:s');
+		$waitingList->whereAdd("expiresAt >= '$now'");
+
+		return $waitingList->find(true);
 	}
 }
