355c8e5a4ef3c645a33a68591cc98c30c3237003
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2026-01-09 11:05:16 +0000

    DIS-1715: feat: add new property to library object
    Add ability for library to allow display of events notifications in user
    accounts to library object

--- code/web/sys/LibraryLocation/Library.php
+++ code/web/sys/LibraryLocation/Library.php
@@ -516,6 +516,7 @@ class Library extends DataObject {
 	// Aspen Events
 	/** @noinspection PhpUnused */
 	public $aspenEventsToInclude;
+	public $displayEventNotificationsInAccount;
 
 	/** @noinspection PhpUnused */
 	public $allowUpdatingHolidaysFromILS;
@@ -3665,7 +3666,15 @@ class Library extends DataObject {
 							'2' => "Events that occur at one of this library's locations",
 						],
 						'default' => '2',
-					]
+					],
+					'displayEventNotificationsInAccount' => [
+						'property' => 'displayEventNotificationsInAccount',
+						'type' => 'checkbox',
+						'label' => 'Display Event Notifications in Account',
+						'description' => 'Whether or not to display a notification banner in the user\' account when they are eligible to register for an event for which they were on the waiting list',
+						'hideInLists' => true,
+						'default' => 1,
+					],
 				]
 			],
 
