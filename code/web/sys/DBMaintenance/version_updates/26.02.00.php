33ab718f52bac565cf1c1d2309b5eda59612a4db
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2026-01-15 13:06:53 +0000

    DIS-1555: feat: update db
    -Add new permission for community engagement
    -Move Community Engagement Permissions to their own section in
    permissions

--- code/web/sys/DBMaintenance/version_updates/26.02.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.02.00.php
@@ -26,6 +26,26 @@ function getUpdates26_02_00(): array {
 		//galen
 
 		//alexander
+		'add_admin_view_permission_to_community_engagement' => [
+			'title' => 'Add Admin View Permission to Community Engagement',
+			'description' => 'Add a new permission for admin view page for commnuity engagement',
+			'continueOnError' => false,
+			'sql' => [
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Community Engagement', 'View Community Engagement Admin View', 'Community Engagement', 200, 'Allows the user to view the Community Engagement Admin View.')",
+				"INSERT INTO role_permissions(roleId, permissionId) VALUES 
+					((SELECT roleId FROM roles WHERE name='opacAdmin'), 
+						(SELECT id FROM permissions WHERE name='View Community Engagement Admin View'))"
+			]
+		],// add_admin_view_permission_to_community_engagement
+		'community_engagement_section_rename' => [
+			'title' => 'Community Engagement - Move Permissions to Community Engagement Section',
+			'description' => 'Updates the sectionName for Community Engagement permissions from their current sections to Community Engagement',
+			'continueOnError' => false,
+			'sql' => [
+				"UPDATE permissions SET sectionName = 'Community Engagement' WHERE name = 'View Community Engagement Dashboard'",
+				"UPDATE permissions SET sectionName = 'Community Engagement' WHERE name = 'Administer Community Engagement Module'"
+			]
+		], //community_engagement_section_rename
 
 		//chloe
 
