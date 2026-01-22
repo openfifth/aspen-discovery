d6169dc2d37ba53317cab4263d9ca1048753db5f
Mark Jarrell <mjarrell@richlandlibrary.com>
2026-01-21 22:31:40 -0500

    Removed necessity to tab through browse category pagination links.

--- code/web/interface/themes/responsive/js/aspen.js
+++ code/web/interface/themes/responsive/js/aspen.js
@@ -1164,7 +1164,7 @@ var AspenDiscovery = (function(){
 					.jcarouselPagination({
 						perPage: 1,
 						item: function(page) {
-							return '<a href="#' + page + '" role="button" tabindex="0">' + page + '</a>';
+							return '<a href="#' + page + '" role="button" tabindex="-1">' + page + '</a>';
 						}
 					});
 
