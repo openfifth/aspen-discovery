7b2ab2abeaffc6285a2582d3779f75f86a6bde95
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-01-22 18:44:42 +0000

    DIS-1597: feat: save registration fields user input on registration
    (cherry picked from commit 4cecdc66a0ee756fd62432087ed5c5e4d560ee3d)
    Signed-off-by: Chloé Zermatten <chloe.zermatten@openfifth.co.uk>

--- code/web/interface/themes/responsive/AspenEvents/customRegistrationForm.tpl
+++ code/web/interface/themes/responsive/AspenEvents/customRegistrationForm.tpl
@@ -1,7 +1,8 @@
-<div id="eventRegistrationFormContainer">
+<form id="eventRegistrationForm-{$eventSourceId}" method="post">
+<form method="post" id="eventRegistrationForm" name="eventRegistrationForm" class="form-horizontal" role="form" onsubmit="return AspenDiscovery.Account.saveEventRegistrationInformation()">
     {foreach from=$registrationFormStructure item=property}
-		{if is_array($property) && isset($property.property) && isset($property.type)}
-			{include file="DataObjectUtil/property.tpl"}
-		{/if}
-	{/foreach}
-</div>
+    	{if is_array($property) && isset($property.property) && isset($property.type)}
+    		{include file="DataObjectUtil/property.tpl"}
+    	{/if}
+    {/foreach}
+</form>
--- code/web/interface/themes/responsive/js/aspen/account.js
+++ code/web/interface/themes/responsive/js/aspen/account.js
@@ -2377,11 +2377,15 @@ AspenDiscovery.Account = (function () {
 			const userIdField = document.getElementById(`eventRegistrationUserId-${eventSourceId}`);
 			const userId = userSelector ? userSelector.value : (userIdField ? userIdField.value : null);
 
+			const informationForm = document.getElementById(`eventRegistrationForm-${eventSourceId}`);
+			const registrationInformation = Object.fromEntries(new FormData(informationForm).entries());
+
 			const url = Globals.path + "/MyAccount/AJAX";
 			const params = {
 				method: 'toggleUserRegistrationToEvent',
 				eventInstanceId: eventSourceId.replace(/aspenEvent_\d+_/, ''),
-				userId
+				userId,
+				...registrationInformation,
 			};
 
 			$.getJSON(url, params, function (data) {
