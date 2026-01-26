25a3b4db70008a14fe0a1eb132975c4e4a7f988e
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2026-01-26 16:07:06 +0000

    RMAINT: db updates

--- code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
@@ -3,6 +3,15 @@
 /** @noinspection PhpUnused */
 function getUpdates26_Q1_00(): array {
 	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
 		//yanjun
 		'overdrive_suppress_kindle_format' => [
 			'title' => 'OverDrive Suppress Kindle Format',
@@ -12,6 +21,7 @@ function getUpdates26_Q1_00(): array {
 				"ALTER TABLE overdrive_scopes ADD COLUMN suppressKindleFormat TINYINT(1) DEFAULT 0",
 			]
 		],
+
 		// tomas
 		'async_facet_loading' => [
 			'title' => 'Async Facet Loading Configuration',
@@ -21,5 +31,20 @@ function getUpdates26_Q1_00(): array {
 				'ALTER TABLE library ADD COLUMN IF NOT EXISTS enableAsyncFacetLoading TINYINT(1) DEFAULT 1 COMMENT "Enable async loading of collapsed facets to improve initial search performance" AFTER groupedWorkDisplaySettingId'
 			]
 		],
+
+        //lucas
+		'indexing_profiles_add_default_values' => [
+			'title' => 'Indexing Profiles - Add Default Values',
+			'description' => 'Add default values to required columns in indexing_profiles table to support multi-step form creation via UI.',
+			'continueOnError' => false,
+			'sql' => [
+				"ALTER TABLE indexing_profiles MODIFY COLUMN recordNumberTag char(3) NOT NULL DEFAULT ''",
+				"ALTER TABLE indexing_profiles MODIFY COLUMN recordNumberPrefix varchar(10) NOT NULL DEFAULT ''",
+				"ALTER TABLE indexing_profiles MODIFY COLUMN itemTag char(3) NOT NULL DEFAULT ''",
+				"ALTER TABLE indexing_profiles MODIFY COLUMN marcPath varchar(100) NOT NULL DEFAULT ''",
+				"ALTER TABLE indexing_profiles MODIFY COLUMN indexingClass varchar(50) NOT NULL DEFAULT ''",
+			]
+		], //indexing_profiles_add_default_values
+		
 	];
 }
