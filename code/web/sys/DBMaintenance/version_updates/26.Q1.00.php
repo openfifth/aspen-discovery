adad140f870a5cd64ee232297bd3ca31e23290c7
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2026-01-26 16:07:06 +0000

    RMAINT: db updates

--- code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
@@ -1,9 +1,18 @@
 <?php
 
 /** @noinspection PhpUnused */
-function getUpdates26_01_00(): array {
+function getUpdates26_Q1_00(): array {
 	return [
-	//lucas
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+        //lucas
 		'indexing_profiles_add_default_values' => [
 			'title' => 'Indexing Profiles - Add Default Values',
 			'description' => 'Add default values to required columns in indexing_profiles table to support multi-step form creation via UI.',
@@ -16,6 +25,6 @@ function getUpdates26_01_00(): array {
 				"ALTER TABLE indexing_profiles MODIFY COLUMN indexingClass varchar(50) NOT NULL DEFAULT ''",
 			]
 		], //indexing_profiles_add_default_values
-
+		
 	];
 }
