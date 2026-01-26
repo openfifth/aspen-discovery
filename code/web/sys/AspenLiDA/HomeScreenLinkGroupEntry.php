9d3b24ea995c051e879026df7085da63e85c5639
kirstien <kirstienkroeger@icloud.com>
2026-01-25 19:30:03 -0600

    Update HomeScreenLinkGroupEntry.php

--- code/web/sys/AspenLiDA/HomeScreenLinkGroupEntry.php
+++ code/web/sys/AspenLiDA/HomeScreenLinkGroupEntry.php
@@ -91,200 +91,77 @@ class HomeScreenLinkGroupEntry extends DataObject {
 		return self::$_objectStructure[$context];
 	}
 
-	/** @noinspection PhpUnusedParameterInspection */
-	public function getEditLink(string $context): string {
-		return '/Admin/BrowseCategories?objectAction=edit&id=' . $this->browseCategoryId;
-	}
+	protected $_homeScreenLink = null;
 
-	protected $_browseCategory = null;
-
-	function getBrowseCategory(): BrowseCategory|false {
-		if ($this->_browseCategory == null) {
-			require_once ROOT_DIR . '/sys/Browse/BrowseCategory.php';
-			$this->_browseCategory = new BrowseCategory();
-			$this->_browseCategory->id = $this->browseCategoryId;
-			if (!$this->_browseCategory->find(true)) {
-				$this->_browseCategory = false;
+	function getHomeScreenLink(): HomeScreenLink|false {
+		if ($this->_homeScreenLink == null) {
+			require_once ROOT_DIR . '/sys/AspenLiDA/HomeScreenLink.php';
+			$this->_homeScreenLink = new HomeScreenLink();
+			$this->_homeScreenLink->id = $this->homeScreenLinkId;
+			if (!$this->_homeScreenLink->find(true)) {
+				$this->_homeScreenLink = false;
 			}
 		}
-		return $this->_browseCategory;
+
+		return $this->_homeScreenLink;
+	}
+
+	/** @noinspection PhpUnusedParameterInspection */
+	public function getEditLink(string $context): string {
+		return '/AspenLiDA/HomeScreenLink?objectAction=edit&id=' . $this->homeScreenLinkId;
 	}
 
 	public function canActiveUserChangeSelection(): bool {
-		if (UserAccount::userHasPermission('Administer Selected Browse Category Groups')) {
-			//Always allow since the only way they can get here is by editing a group they have access to
+		$library = Library::getPatronHomeLibrary(UserAccount::getActiveUserObj());
+		$libraryId = $library == null ? -1 : $library->libraryId;
+		$linkId = $this->getHomeScreenLink()->libraryId;
+		if (($this->getHomeScreenLink()->sharing == 'everyone') || (UserAccount::userHasPermission('Administer All Aspen LiDA Home Screen Links'))) {
 			return true;
-		} else {
-			$library = Library::getPatronHomeLibrary(UserAccount::getActiveUserObj());
-			$libraryId = $library == null ? -1 : $library->libraryId;
-			$browseCatId = $this->getBrowseCategory()->libraryId;
-			if (($this->getBrowseCategory()->sharing == 'everyone') || (UserAccount::userHasPermission('Administer All Browse Categories'))) {
-				return true;
-			} else if ($browseCatId == $libraryId) {
-				return UserAccount::userHasPermission('Administer Library Browse Categories');
-			}
+		} else if ($linkId == $libraryId) {
+			return UserAccount::userHasPermission('Administer Library Aspen LiDA Home Screen Links');
 		}
 		return false;
 	}
 
 	public function canActiveUserDelete(): bool {
-		return UserAccount::userHasPermission('Administer All Browse Categories') || UserAccount::userHasPermission('Administer Library Browse Categories') || UserAccount::userHasPermission('Administer Selected Browse Category Groups');
+		return UserAccount::userHasPermission('Administer All Aspen LiDA Home Screen Links') || UserAccount::userHasPermission('Administer Library Aspen LiDA Home Screen Links');
 	}
 
 	public function canActiveUserEdit(): bool {
-		if (UserAccount::userHasPermission('Administer Selected Browse Category Groups')) {
-			//Always allow since the only way they can get here is by editing a group they have access to
+		$library = Library::getPatronHomeLibrary(UserAccount::getActiveUserObj());
+		$libraryId = $library == null ? -1 : $library->libraryId;
+		$linkId = $this->getHomeScreenLink()->libraryId;
+		if (($this->getHomeScreenLink()->sharing == 'everyone') || (UserAccount::userHasPermission('Administer All Aspen LiDA Home Screen Links'))) {
 			return true;
-		} else {
-			$library = Library::getPatronHomeLibrary(UserAccount::getActiveUserObj());
-			$libraryId = $library == null ? -1 : $library->libraryId;
-			$browseCatId = $this->getBrowseCategory()->libraryId;
-			if (($this->getBrowseCategory()->sharing == 'everyone') || (UserAccount::userHasPermission('Administer All Browse Categories'))) {
-				return true;
-			} elseif ($browseCatId == $libraryId) {
-				return UserAccount::userHasPermission('Administer Library Browse Categories');
-			}
+		} elseif ($linkId == $libraryId) {
+			return UserAccount::userHasPermission('Administer Library Aspen LiDA Home Screen Links');
 		}
 		return false;
 	}
 
 	public function toArray($includeRuntimeProperties = true, $encryptFields = false): array {
-		//Unset ids for group and browse category since they will be set by links
 		$return = parent::toArray($includeRuntimeProperties, $encryptFields);
-		unset($return['browseCategoryGroupId']);
-		unset($return['browseCategoryId']);
+		unset($return['homeScreenLinkGroupId']);
+		unset($return['homeScreenLinkId']);
 		return $return;
 	}
 
 	public function getLinksForJSON(): array {
 		$links = parent::getLinksForJSON();
-		$browseCategory = $this->getBrowseCategory();
-		$browseCategoryArray = $browseCategory->toArray();
-		$browseCategoryArray['links'] = $browseCategory->getLinksForJSON();
-		$links['browseCategory'] = $browseCategoryArray;
+		$link = $this->getHomeScreenLink();
+		$linksArray = $link->toArray();
+		$linksArray['links'] = $link->getLinksForJSON();
+		$links['homeScreenLinks'] = $linksArray;
 		return $links;
 	}
 
 	public function loadEmbeddedLinksFromJSON($jsonData, $mappings, string $overrideExisting = 'keepExisting'): void {
 		parent::loadRelatedLinksFromJSON($jsonData, $mappings, $overrideExisting);
-		if (array_key_exists('browseCategory', $jsonData)) {
-			require_once ROOT_DIR . '/sys/Browse/BrowseCategory.php';
-			$browseCategory = new BrowseCategory();
-			$browseCategory->loadFromJSON($jsonData['browseCategory'], $mappings, $overrideExisting);
-			$this->browseCategoryId = $browseCategory->id;
+		if (array_key_exists('homeScreenLinks', $jsonData)) {
+			require_once ROOT_DIR . '/sys/AspenLiDA/HomeScreenLink.php';
+			$homeScreenLink = new HomeScreenLink();
+			$homeScreenLink->loadFromJSON($jsonData['homeScreenLinks'], $mappings, $overrideExisting);
+			$this->homeScreenLinkId = $homeScreenLink->id;
 		}
-	}
-
-	public function isDismissed(): bool {
-		require_once ROOT_DIR . '/sys/Browse/BrowseCategory.php';
-		require_once ROOT_DIR . '/sys/Browse/BrowseCategoryDismissal.php';
-		if (UserAccount::isLoggedIn()) {
-			$browseCategory = new BrowseCategory();
-			$browseCategory->id = $this->browseCategoryId;
-			if ($browseCategory->find(true)) {
-				$browseCategoryDismissal = new BrowseCategoryDismissal();
-				$browseCategoryDismissal->browseCategoryId = $browseCategory->textId;
-				$browseCategoryDismissal->userId = UserAccount::getActiveUserId();
-				if ($browseCategoryDismissal->find(true)) {
-					return true;
-				}
-			}
-			return false;
-		}
-		return false;
-	}
-
-	public function isValidForDisplay($appUser = null, $checkDismiss = true): bool {
-		require_once ROOT_DIR . '/sys/Browse/BrowseCategory.php';
-		$browseCategory = new BrowseCategory();
-		$browseCategory->id = $this->browseCategoryId;
-
-		if ($browseCategory->find(true)) {
-			$curTime = time();
-			if ($browseCategory->startDate != 0 && $browseCategory->startDate > $curTime) {
-				return false;
-			}
-			if ($browseCategory->endDate != 0 && $browseCategory->endDate < $curTime) {
-				return false;
-			}
-
-			$user = UserAccount::getActiveUserObj();
-
-			if ($browseCategory->textId == 'system_user_lists' || $browseCategory->textId == 'system_saved_searches' || $browseCategory->textId == 'system_recommended_for_you') {
-				if (UserAccount::isLoggedIn()) {
-					if ($browseCategory->textId == 'system_saved_searches' && $user->hasSavedSearches()) {
-						if ($checkDismiss) {
-							if ($this->isDismissed()) {
-								return false;
-							}
-						}
-						return true;
-					}
-					if ($browseCategory->textId == 'system_user_lists' && $user->hasLists()) {
-						if ($checkDismiss) {
-							if ($this->isDismissed()) {
-								return false;
-							}
-						}
-						return true;
-					}
-					if ($browseCategory->textId == 'system_recommended_for_you' && $user->hasRatings()) {
-						if ($checkDismiss) {
-							if ($this->isDismissed()) {
-								return false;
-							}
-						}
-						return true;
-					}
-
-				}
-				return false;
-			}
-		}
-
-		if ($checkDismiss) {
-			if (UserAccount::isLoggedIn()) {
-				if ($this->isDismissed()) {
-					return false;
-				}
-			}
-		}
-
-		return true;
-	}
-
-	public function isValidForDisplayInApp($user, $checkDismiss = false): bool {
-		require_once ROOT_DIR . '/sys/Browse/BrowseCategory.php';
-		$browseCategory = new BrowseCategory();
-		$browseCategory->id = $this->browseCategoryId;
-
-		if ($browseCategory->find(true)) {
-			$curTime = time();
-			if ($browseCategory->startDate != 0 && $browseCategory->startDate > $curTime) {
-				return false;
-			}
-			if ($browseCategory->endDate != 0 && $browseCategory->endDate < $curTime) {
-				return false;
-			}
-
-			if ($checkDismiss && ($user && !($user instanceof AspenError))) {
-				require_once ROOT_DIR . '/sys/Browse/BrowseCategoryDismissal.php';
-				$browseCategoryDismissal = new BrowseCategoryDismissal();
-				$browseCategoryDismissal->browseCategoryId = $browseCategory->textId;
-				$browseCategoryDismissal->userId = $user->id;
-				if ($browseCategoryDismissal->find(true)) {
-					return false;
-				}
-			}
-
-			if ($browseCategory->textId == 'system_user_lists' || $browseCategory->textId == 'system_saved_searches' || $browseCategory->textId == 'system_recommended_for_you') {
-				if (!$user || ($user instanceof AspenError)) {
-					return false;
-				}
-			}
-
-			return true;
-		}
-		return false;
 	}
 }
