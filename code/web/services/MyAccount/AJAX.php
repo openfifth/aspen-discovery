db743345ad193eeab6f125996f2b82328a1a1264
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2026-01-28 10:56:58 +0000

    DIS-1836: fix: fix variable name

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -12679,7 +12679,7 @@ class MyAccount_AJAX extends JSON_Action {
 
 					$data = [
 						'id' => 'event_change_' . $eventNotifications->id,
-						'notificationId' =>$notification->id,
+						'notificationId' =>$eventNotifications->id,
 						'type' => 'event_change',
 						'title' => $title,
 						'body' => translate([
