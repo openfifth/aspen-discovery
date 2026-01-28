c75b6ed1025eac29f46040075518fd096a6aec5c
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2026-01-28 13:07:18 +0000

    DIS-1715: refactor: Add limit for notifications

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -12048,6 +12048,7 @@ class MyAccount_AJAX extends JSON_Action {
 			$waitingList->toastShown = 0;
 			$waitingList->whereAdd("expiresAt >= NOW()");
 			$waitingList->orderBy('expiresAt ASC');
+			$waitingList->limit(10);
 
 			if ($waitingList->find()) {
 				while ($waitingList->fetch()) {
@@ -12108,6 +12109,7 @@ class MyAccount_AJAX extends JSON_Action {
 			$eventNotifications->userId = $patron->id;
 			$eventNotifications->toastShown = 0;
 			$eventNotifications->orderBy('createdAt ASC');
+			$eventNotifications->limit(10);
 
 			if ($eventNotifications->find()) {
 				while ($eventNotifications->fetch()) {
