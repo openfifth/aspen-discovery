0ccfac9a0e950624f3018f474d7e2fb9b4cf7675
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2026-02-04 16:51:38 +0000

    DIS-1565: feat: update db

--- code/web/sys/DBMaintenance/version_updates/26.03.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.03.00.php
@@ -28,6 +28,14 @@ function getUpdates26_03_00(): array {
 		//galen
 
 		//alexander
+		'add_option_to_add_location_to_event_thumbail_image' => [
+			'title' => 'Add Option to Add Location to Event Thumnail Image',
+			'description' => 'Add ability to choose to add event location to event thumbnail image',
+			'sql' => [
+				"ALTER TABLE event ADD COLUMN displayEventBranchOnThumbnail TINYINT(1) DEFAULT 0",
+				"ALTER TABLE user_events_entry ADD COLUMN displayEventBranchOnThumbnail TINYINT(1) DEFAULT 0"
+			]
+		], //add_option_to_add_location_to_event_thumbnail_image 
 
 		//chloe
 
