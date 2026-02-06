7ffb167554db7485931401e51f09c80e618fccae
Mark Noble <mdnoble73@gmail.com>
2026-02-06 08:44:44 -0700

    Add default files for 26.03

---
+++ code/web/sys/DBMaintenance/version_updates/26.03.00.php
@@ -0,0 +1,45 @@
+<?php
+/** @noinspection SqlDialectInspection */
+
+/** @noinspection PhpUnused */
+function getUpdates26_03_00(): array {
+	$now = time();
+
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark n
+
+		//kirstien
+
+		//kodi
+
+		//yanjun
+
+		//imani
+
+		//galen
+
+		//alexander
+
+		//chloe
+
+		//mark j
+
+		//lucas
+
+
+		//tomas
+
+		//other
+
+
+	];
+}
