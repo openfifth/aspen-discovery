540ef63de39f600b6f7fa92f4677b14d3dc6b1b5
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-02-13 20:00:40 +0000

    feat(api): add dynamic OpenAPI spec aggregation
    merged_openapi.php combines base spec with all *_openapi.json files.
    Enables single source of truth for per-API documentation.
    
    (cherry picked from commit 83d4d05a1380d02df7f81e7c2bf6433c181b0f6b)
    Signed-off-by: Chloé Zermatten <chloe.zermatten@openfifth.co.uk>

--- code/web/services/API/Documentation.php
+++ code/web/services/API/Documentation.php
@@ -7,38 +7,11 @@ class API_Documentation extends Action {
     function launch() : void {
         global $interface;
 
-        $openApiPath = ROOT_DIR . '/openapi';
-        $apiFiles = [];
+        $apiFile = "/openapi/merged_openapi.php";
 
-        if (is_dir($openApiPath)) {
-            foreach (scandir($openApiPath) as $apiFile) {
-                if (preg_match('/_openapi\.json$/i', $apiFile) && is_file($openApiPath . '/' . $apiFile)) {
-                    $apiFiles[str_ireplace('_openapi.json', '', $apiFile)] = $apiFile;
-                }
-            }
-        }
-
-        if (empty($apiFiles)) {
-            $activeApiFile = '';
-            $activeApiFileFullPath = '';
-        } else {
-            if (isset($_REQUEST['api']) && array_key_exists($_REQUEST['api'], $apiFiles)) {
-                $activeApiFile = $_REQUEST['api'];
-            } else {
-                $keys = array_keys($apiFiles);
-                $activeApiFile = $keys[0];
-            }
-            $activeApiFileFullPath = '/openapi/' . $apiFiles[$activeApiFile];
-        }
-
-        $apiBasePath = "/API/$activeApiFile?method=";
-
-        $interface->assign('apiFiles', $apiFiles);
-        $interface->assign('activeApiFile', $activeApiFile);
-        $interface->assign('activeApiFileFullPath', $activeApiFileFullPath);
-        $interface->assign('apiBasePath', $apiBasePath);
         $interface->assign('showBreadcrumbs', true);
         $interface->assign('showContentAsFullWidth', true);
+        $interface->assign('apiFile', $apiFile);
 
         if (UserAccount::isLoggedIn() && count(UserAccount::getActivePermissions()) > 0) {
             $adminActions = UserAccount::getActiveUserObj()->getAdminActions();
