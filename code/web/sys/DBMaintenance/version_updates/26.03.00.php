4b028f302fdf0d169fd59e65669b7459cc5dfd45
stephenrwicks <stephenrwicks@gmail.com>
2026-02-13 08:40:25 -0500

    Add sql

--- code/web/sys/DBMaintenance/version_updates/26.03.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.03.00.php
@@ -39,13 +39,23 @@ function getUpdates26_03_00(): array {
 		//tomas
 
 		// stephen
-
 		'add_success_button_to_theme' => [
-			'title' => 'Add Success button to theme options',
-			'description' => 'Add configuration to the Theme page for the Bootstrap Success button',
+			'title' => 'Theme - Add Success button customization',
+			'description' => 'In Themes, libraries can now customize the Bootstrap Success button.',
 			'continueOnError' => true,
 			'sql' => [
-				//"ALTER TABLE Theme ADD COLUMN successButtonBackgroundColor varchar(7) DEFAULT #",
+				"ALTER TABLE themes ADD COLUMN successButtonBackgroundColor char(7) DEFAULT '#5cb85c'",
+				"ALTER TABLE themes ADD COLUMN successButtonBackgroundColorDefault tinyint(1) DEFAULT 1",
+				"ALTER TABLE themes ADD COLUMN successButtonForegroundColor char(7) DEFAULT '#000000'",
+				"ALTER TABLE themes ADD COLUMN successButtonForegroundColorDefault tinyint(1) DEFAULT 1",
+				"ALTER TABLE themes ADD COLUMN successButtonBorderColor char(7) DEFAULT '#4cae4c'",
+				"ALTER TABLE themes ADD COLUMN successButtonBorderColorDefault tinyint(1) DEFAULT 1",
+				"ALTER TABLE themes ADD COLUMN successButtonHoverBackgroundColor char(7) DEFAULT '#449d44'",
+				"ALTER TABLE themes ADD COLUMN successButtonHoverBackgroundColorDefault tinyint(1) DEFAULT 1",
+				"ALTER TABLE themes ADD COLUMN successButtonHoverForegroundColor char(7) DEFAULT '#000000'",
+				"ALTER TABLE themes ADD COLUMN successButtonHoverForegroundColorDefault tinyint(1) DEFAULT 1",
+				"ALTER TABLE themes ADD COLUMN successButtonHoverBorderColor char(7) DEFAULT '#398439'",
+				"ALTER TABLE themes ADD COLUMN successButtonHoverBorderColorDefault tinyint(1) DEFAULT 1",
 			]
 		],
 
