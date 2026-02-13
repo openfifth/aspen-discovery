08e139aceb46068c79703bf46050dafd66413791
AlexanderBlanchardAC <alexanderblanchardac@yahoo.com>
2026-02-13 17:16:42 +0000

    RMAINT: DB Updates

--- code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
@@ -33,5 +33,23 @@ function getUpdates26_Q1_00(): array {
 				"ALTER TABLE library ADD COLUMN workPhoneField CHAR(1) DEFAULT 'p'"
 			]
 		], //sierra_phone_fields
+		//mark n
+		'ip_address_bypass_failed_login_checks' => [
+			'title' => 'IP Address Bypass Failed Login checks',
+			'description' => 'Add Bypass Failed Login checks to IP Address settings',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE ip_lookup ADD COLUMN bypassFailedLoginChecks TINYINT(1) DEFAULT 0',
+			]
+		], //ip_address_bypass_failed_login_checks
+		'sierra_address_line_for_city_state_zip' => [
+			'title' => 'Sierra Address Line for City State Zip',
+			'description' => 'Allow the address line which is used for City State Zip to be defined',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE library ADD COLUMN sierraAddressLineForCityState TINYINT(1) DEFAULT 2',
+				'ALTER TABLE library ADD COLUMN sierraZipOnSameLineAsCityState TINYINT(1) DEFAULT 1',
+			]
+		], //sierra_address_line_for_city_state_zip
 	];
 }
