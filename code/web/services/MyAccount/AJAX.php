5b28e2af6e239eafad53abf03d7a0c330d4aadfd
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-02-27 17:41:42 +0000

    DIS-1909: fix: prevent smarty errors

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -8456,7 +8456,7 @@ class MyAccount_AJAX extends JSON_Action {
 			$userWaitingList->whereAdd('status IN ("waiting", "notified")');
 			$userOnWaitingList = $userWaitingList->find(true);
 			$userWaitingListPosition = null;
-
+			$userCanRegister = false;
 
 			if ($userOnWaitingList) {
 				$userOnWaitingList = true;
@@ -8469,6 +8469,7 @@ class MyAccount_AJAX extends JSON_Action {
 			$interface->assign('userCanRegister', $userCanRegister);
 			$interface->assign('userOnWaitingList', $userOnWaitingList);
 			$interface->assign('userWaitingListPosition', $userWaitingListPosition);
+			$interface->assign('userIsRegistered', false);
 
 			$user = UserAccount::getLoggedInUser();
 			if (empty($user)) {
