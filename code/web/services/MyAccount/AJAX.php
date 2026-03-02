e39f835d36af18fedf3bcb485f5fc3a419edf441
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-03-02 12:03:17 +0000

    DIS-1909: fix: honour user email preference
    (cherry picked from commit b25160ce101537dbbcba12fdb10dc11dc16622c4)
    Signed-off-by: Chloé Zermatten <chloe.zermatten@openfifth.co.uk>

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -12518,7 +12518,10 @@ class MyAccount_AJAX extends JSON_Action {
 			if (!($user->find(true))) {
 				continue;
 			}
-			$this->sendEventEmail($user, $changeType, $userInstances);
+
+			if ($user->eventRegistrationNotificationsByEmail == 1) {
+				$this->sendEventEmail($user, $changeType, $userInstances);
+			}
 
 			$homeLibrary = Library::getPatronHomeLibrary();
 			if (is_null($homeLibrary)){
@@ -12574,6 +12577,9 @@ class MyAccount_AJAX extends JSON_Action {
 			'changeType' => $changeType, 
 			'instances' => $formattedInstances, 
 		];
+
+		global $logger;
+		$logger->log('sendEmail called for use with Id: ' . $user->id, Logger::LOG_ERROR);
 
 			$emailTemplate->sendEmail($user->email, $parameters);
 	}
