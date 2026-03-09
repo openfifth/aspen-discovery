7b86de397e8e815ff793c01942722e045c115d12
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-03-09 14:22:06 +0000

    RMAINT: chore: db update

--- code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
@@ -68,5 +68,15 @@ function getUpdates26_Q1_00(): array {
 				"ALTER TABLE library MODIFY COLUMN aspenEventsToInclude INT DEFAULT 0",
 			],
 		], //update_aspenEventsToInclude_default
+
+		// 26_Q1.o5th customer-specific backports
+        'add_option_to_add_location_to_event_thumbail_image' => [
+            'title' => 'Add Option to Add Location to Event Thumnail Image',
+            'description' => 'Add ability to choose to add event location to event thumbnail image',
+            'sql' => [
+                "ALTER TABLE event ADD COLUMN displayEventBranchOnThumbnail TINYINT(1) DEFAULT 0",
+                "ALTER TABLE user_events_entry ADD COLUMN displayEventBranchOnThumbnail TINYINT(1) DEFAULT 0"
+            ]
+        ], //add_option_to_add_location_to_event_thumbnail_image
 	];
 }
