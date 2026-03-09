8ee2dbb65c5da67f05e60379c1e4aa07ea0a7ed2
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-03-09 14:29:43 +0000

    RMAINT: chore: db updates

--- code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.Q1.00.php
@@ -78,5 +78,19 @@ function getUpdates26_Q1_00(): array {
                 "ALTER TABLE user_events_entry ADD COLUMN displayEventBranchOnThumbnail TINYINT(1) DEFAULT 0"
             ]
         ], //add_option_to_add_location_to_event_thumbnail_image
+        'add_option_to_set_display_event_location_on_event_type' => [
+            'title' => 'Add Option to Set Display Event Location On Event Type',
+            'description' => 'Add ability to choose to add event location to event thumbnail image at the event type level',
+            'sql' => [
+                "ALTER TABLE event_type ADD COLUMN displayEventBranchOnThumbnail TINYINT(1) DEFAULT 0",
+            ]
+        ], //add_option_to_set_display_event_location_on_event_type
+		'add_option_to_set_customizability_of_display_event_location_on_event_type' => [
+            'title' => 'Add Option to Set Customizability Of Display Event Location On Event Type',
+            'description' => 'Add ability to choose the customizability of including event location to event thumbnail image at the event type level',
+            'sql' => [
+                "ALTER TABLE event_type ADD COLUMN displayEventBranchOnThumbnailCustomizable TINYINT(1) DEFAULT 0",
+            ]
+        ], //add_option_to_set_customizability_of_display_event_location_on_event_type
 	];
 }
