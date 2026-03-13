b529a7f1ec48621dfe422a37edd687355c04cafb
kirstien <kirstienkroeger@icloud.com>
2026-03-12 21:01:38 -0500

    DIS-1556: Add permissions

--- code/web/sys/DBMaintenance/version_updates/26.04.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.04.00.php
@@ -18,7 +18,18 @@ function getUpdates26_04_00(): array {
 		//mark n
 
 		//kirstien
-
+		'list_transfer_permission' => [
+			'title' => 'Add list transfer permission',
+			'description' => 'Create permission for allowing transfer of list ownership.',
+			'continueOnError' => false,
+			'sql' => [
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES
+					('User Lists', 'Transfer Lists', '', 6, 'Allows the user to transfer a list to another staff.')
+				",
+				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name='opacAdmin'), (SELECT id from permissions where name='Transfer Lists'))",
+			],
+		],
+		//list_transfer_permission
 
 		//kodi
 
