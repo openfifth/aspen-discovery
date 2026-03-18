27b86071151470cd8e5af8741ed2368fc37f53b8
Mark Jarrell <mjarrell@richlandlibrary.com>
2026-03-18 15:36:27 -0400

    DIS-1039: Adds database table and frontend changes.

--- code/web/sys/DBMaintenance/version_updates/26.04.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.04.00.php
@@ -39,6 +39,24 @@ function getUpdates26_04_00(): array {
 				'ALTER TABLE web_builder_resource_usage ADD COLUMN pageViewsFromPlacard INT NOT NULL DEFAULT 0',
 			]
 		], //add_pageViewsFromPlacard_to_web_builder_resource_usage
+		'create_placard_usage_table' => [
+			'title' => 'Create placard usage table',
+			'description' => 'Create placard usage table for tracking things like number of times shown and clicks on placards.',
+			'continueOnError' => false,
+			'sql' => [
+				'CREATE TABLE IF NOT EXISTS placard_usage (
+					id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+					instance VARCHAR(100),
+					year INT,
+					month INT,
+					placardName VARCHAR(255),
+					pageViews INT DEFAULT 0,
+					pageViewsByAuthenticatedUsers INT DEFAULT 0,
+					pageViewsInLibrary INT DEFAULT 0,
+					timesShown INT DEFAULT 0
+				) ENGINE=INNODB',
+			]
+		], //create_placard_usage_table
 
 		//lucas
 
