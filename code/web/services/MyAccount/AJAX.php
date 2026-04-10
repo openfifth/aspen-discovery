720125574e13818f65c15e8a34c84242b8f20b40
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-04-10 21:14:14 +0100

    DIS-1909: refactor(waitlist): swap to status = 'registered'
    Where, in the code, registration was denoted by register being true,
    now instead check for status = 'registered'. This is in preparation for
    the use of other statuses such as 'waiting' or 'invited'.
    
    The swap to a single status property seeks to prevent the potential for
    impossible states (had we gone with adding waiting and invited as
    boolean properties, we could have had any combination of true / false
    values, instead of simply 3 states for the status property).

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -8495,12 +8495,10 @@ class MyAccount_AJAX extends JSON_Action {
 				}
 			}
 			$interface->assign('linkedUsers', $linkedUsers);
-			
-			require_once ROOT_DIR . '/RecordDrivers/AspenEventRecordDriver.php';
-			$sourceId = 'aspenEvent_' . $aspenEventSettings->id . '_' . $eventInstanceId;
-			$recordDriver = new AspenEventRecordDriver($sourceId);	
-			$interface->assign('isRegistered', $recordDriver->isUserRegisteredForEvent());
 
+			$isRegistered = $aspenEventInstanceUserRegistration->status === 'registered';
+
+			$interface->assign('userIsRegistered', $isRegistered);
 			$body .= $interface->fetch('AspenEvents/registrationModalContents.tpl');
 			$result['buttons'] =  $interface->fetch('AspenEvents/registrationToggleButton.tpl');
 		}
@@ -8811,6 +8809,20 @@ class MyAccount_AJAX extends JSON_Action {
 			return $result;
 		}
 
+		require_once ROOT_DIR . '/sys/Events/UserAspenEventInstanceRegistration.php';
+		$userAspenEventInstanceRegistration = new UserAspenEventInstanceRegistration();
+		$userAspenEventInstanceRegistration->eventInstanceId = $eventInstanceId;
+		$userAspenEventInstanceRegistration->userId = $userId;
+
+
+		if (!$eventInstance->hasAvailableSeats(1)) {
+			$result['message'] = translate([
+				'text' => 'This event is full. No seats available.',
+				'isPublicFacing' => true
+			]);
+			return $result;
+		}
+
 		// add the event to saved events if it has not yet been saved
 		$recordDriver->saveUserEventEntry($sourceId, $userId);
 
@@ -8834,7 +8846,7 @@ class MyAccount_AJAX extends JSON_Action {
 		}
 
 		// register the user
-		$registration->insert();
+		$userAspenEventInstanceRegistration->registerUser();
 
 		$result['success'] = true;
 		$result['title'] = translate([
