aa08c24915f5aada228f094a57ae457797ddb1a2
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-04-10 13:25:54 +0100

    DIS-1191: chore: move db update to 26.05.00

--- code/web/sys/DBMaintenance/version_updates/26.04.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.04.00.php
@@ -210,23 +210,6 @@ function getUpdates26_04_00(): array {
 			],
 		], //control_display_of_user_dropdown_in_community_engagement_admin_view
 
-		// alexander
-		'add_ability_for_admin_to_control_whether_holds_can_be_grouped' => [
-			'title' => 'Add Ability for Admin to Control Whether Holds Can Be Grouped',
-			'description' => 'Allow admin to control whether holds can be grouped',
-			'sql' => [
-				"ALTER TABLE library ADD COLUMN allowHoldsToBeGrouped TINYINT(1) DEFAULT 0",
-			],
-		], //add_ability_for_admin_to_control_whether_holds_can_be_grouped
-		'add_grouped_hold_id_to_user_hold' => [
-			'title' => 'Add Grouped Hold Id To User Hold',
-			'description' => 'Add grouped hold id and visual hold group id to user hold',
-			'sql' => [
-				"ALTER TABLE user_hold ADD COLUMN holdGroupId INT(11) DEFAULT NULL",
-				"ALTER TABLE user_hold ADD COLUMN visualHoldGroupId VARCHAR(50) DEFAULT NULL",
-			],
-		], //add_grouped_hold_id_to_user_hold
-
 		//mark j
 		'add_pageViewsFromPlacard_to_web_builder_resource_usage' => [
 			'title' => 'Track web resource page views from a placard',
