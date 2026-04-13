4773831c5f96d0cfe00d5aa2fdd7db9f69a8775a
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-04-13 11:46:38 +0100

    DIS-1909: feat(waitlist): process expired registrations
    (cherry picked from commit ebbddd628e71214ad8ee532692299745726a585d)
    Signed-off-by: Chloé Zermatten <chloe.zermatten@openfifth.co.uk>

---
+++ code/web/cron/updateEventRegistrationInvitations.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/../bootstrap_aspen.php';
+require_once ROOT_DIR . '/sys/CronLogEntry.php';
+require_once ROOT_DIR . '/sys/Events/UserAspenEventInstanceRegistration.php';
+require_once ROOT_DIR . '/sys/Events/EventInstance.php';
+
+$cronLogEntry = new CronLogEntry();
+$cronLogEntry->startTime = time();
+$cronLogEntry->name = 'Update Event Registration Invites';
+$cronLogEntry->insert();
+
+$expiredCount = 0;
+$invitedCount = 0;
+
+$expiredRowIds = UserAspenEventInstanceRegistration::getExpiredInvitedRowIds();
+
+$affectedInstanceIds = [];
+
+foreach ($expiredRowIds as $rowId) {
+	$row = new UserAspenEventInstanceRegistration();
+	$row->id = $rowId;
+	if (!$row->find(true)) {
+		continue;
+	}
+	$affectedInstanceIds[$row->eventInstanceId] = true;
+	$row->delete();
+	$expiredCount++;
+}
+
+foreach (array_keys($affectedInstanceIds) as $eventInstanceId) {
+	$eventInstance = new EventInstance();
+	$eventInstance->id = $eventInstanceId;
+	if (!$eventInstance->find(true)) {
+		continue;
+	}
+	if ($eventInstance->inviteNextOnWaitingList()) {
+		$invitedCount++;
+	}
+}
+
+$cronLogEntry->notes = "Expired: $expiredCount, Invited: $invitedCount";
+$cronLogEntry->endTime = time();
+$cronLogEntry->update();
