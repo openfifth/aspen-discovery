235901629a016c5c7acaf7189d21a93322dc15cd
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-04-13 16:00:36 +0100

    DIS-1909: feat(notification): handle user preference update
    (cherry picked from commit de7e9f0175f8c0f9cea8693f79e5a6c5db96409c)
    Signed-off-by: Chloé Zermatten <chloe.zermatten@openfifth.co.uk>

--- code/web/sys/Account/User.php
+++ code/web/sys/Account/User.php
@@ -1782,6 +1782,7 @@ class User extends DataObject {
 
 		$this->__set('optInToAllCampaignLeaderboards', (isset($_POST['optInToAllCampaignLeaderboards']) && $_POST['optInToAllCampaignLeaderboards'] == 'on') ? 1 : 0);
 		$this->__set('campaignNotificationsByEmail', (isset($_POST['campaignNotificationsByEmail']) && $_POST['campaignNotificationsByEmail'] == 'on') ? 1 : 0);
+		$this->__set('eventRegistrationNotificationsByEmail', (isset($_POST['eventRegistrationNotificationsByEmail']) && $_POST['eventRegistrationNotificationsByEmail'] == 'on') ? 1 : 0);
 
 		if ($library->holdPromptForEditions > 0) {
 			if (isset($_POST['rememberHoldPromptForEdition'])) {
