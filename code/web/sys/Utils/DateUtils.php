1e59bd2ae8a23442b15b46815a757cbb917b8b06
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-04-13 14:36:33 +0100

    DIS-1909: feat(notification): date formatting utils

--- code/web/sys/Utils/DateUtils.php
+++ code/web/sys/Utils/DateUtils.php
@@ -13,4 +13,24 @@ class DateUtils {
 		$newdate = date('Y-m-d H:i:s', mktime(date('H', $cd), date('i', $cd) + $minutes, date('s', $cd), date('m', $cd), date('d', $cd), date('Y', $cd)));
 		return $newdate;
 	}
+
+	static function formatHumanDate(string $date): string {
+		$dt = DateTimeImmutable::createFromFormat('Y-m-d', $date);
+
+		if ($dt === false) {
+			return $date;
+		}
+
+		return $dt->format('l jS F Y');
+	}
+
+	static function formatHumanDateTime(string $dateTime): string {
+		$dt = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $dateTime);
+
+		if ($dt === false) {
+			return $dateTime;
+		}
+
+		return $dt->format('l jS F Y \a\t H:i');
+	}
 }
