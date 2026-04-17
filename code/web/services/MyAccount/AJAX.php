548321814893d121f061055ba7f758c1b9394e95
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-04-17 18:56:01 +0100

    DIS-1597: feat(multi-attendees): surface data to interface

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -8369,6 +8369,7 @@ class MyAccount_AJAX extends JSON_Action {
 			$eventType = $eventInstance->getEventType();
 			$registrationFormStructure = $eventType->getFieldSetFieldsByUse(2);
 			$interface->assign('registrationFormStructure', $registrationFormStructure);
+			$interface->assign('attendeeCategories', $eventType->getEventTypeAttendeeCategories());
 
 			$body .= $interface->fetch('AspenEvents/registrationModalContents.tpl');
 			$result['buttons'] =  $interface->fetch('AspenEvents/registrationToggleButton.tpl');
