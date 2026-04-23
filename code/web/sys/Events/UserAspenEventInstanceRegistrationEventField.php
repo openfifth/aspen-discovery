5308c8f5ac18cf78072e7219aa00d483dd5e7e1c
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-04-23 12:03:47 +0100

    DIS-1597: feat(custom-fields): get values for a reg

--- code/web/sys/Events/UserAspenEventInstanceRegistrationEventField.php
+++ code/web/sys/Events/UserAspenEventInstanceRegistrationEventField.php
@@ -6,4 +6,15 @@ class UserAspenEventInstanceRegistrationEventField extends DataObject {
 	public $eventInstanceRegistrationId;
 	public $eventFieldId;
 	public $value;
+
+	public static function getValuesForRegistration(int $registrationId): array {
+		$field = new UserAspenEventInstanceRegistrationEventField();
+		$field->eventInstanceRegistrationId = $registrationId;
+		$field->find();
+		$values = [];
+		while ($field->fetch()) {
+			$values[(int)$field->eventFieldId] = $field->value;
+		}
+		return $values;
+	}
 }
