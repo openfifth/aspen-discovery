c936206f088c6cc1680ea1689d9eb3e497d35282
Mark Jarrell <mark.jarrell@gmail.com>
2026-04-30 18:46:47 -0400

    DIS-2334: Updates user agent capture to only store the first part of the agent (no browser version info).

--- code/web/bootstrap.php
+++ code/web/bootstrap.php
@@ -119,6 +119,8 @@ global $userAgent;
 $userAgentString = 'Unknown';
 if (isset($_SERVER['HTTP_USER_AGENT'])) {
 	$userAgentString = $_SERVER['HTTP_USER_AGENT'];
+	// Remove everything after the first "/" to avoid storing version information.
+	$userAgentString = substr($userAgentString, 0, strpos($userAgentString, "/"));
 }
 try {
 	// Check if user agent tracking is disabled
