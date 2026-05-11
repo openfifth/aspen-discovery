0f418064f2b1f9b2313f44b31090bfc360fc32da
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-05-11 18:43:34 +0100

    DIS-XXXX: fix: mark patron as found
    The Koha driver's processEmailResetPinForm was not setting foundPatron
    appropriately, resulting in Aspen proceeding to check the DB for users
    it was not going to find (assigning the ils_barcode as the username
    which is incorrect, and setting source as admin, which is also
    incorrect)
    
    By setting foundPatron, we trigger the break in processEmailResetPinForm
    in code/web/CatalogConnection.php.

--- code/web/Drivers/Koha.php
+++ code/web/Drivers/Koha.php
@@ -3800,7 +3800,7 @@ class Koha extends AbstractIlsDriver {
 	function processEmailResetPinForm() : array {
 		$result = [
 			'success' => false,
-			'patronFound' => false,
+			'foundPatron' => false,
 			'error' => translate([
 				'text' => "Unknown error sending password reset.",
 				'isPublicFacing' => true,
@@ -3887,11 +3887,13 @@ class Koha extends AbstractIlsDriver {
 						$error);
 				}
 				$result['error'] = trim($error);
+				$result['foundPatron'] = true;
 			}
 			elseif (preg_match('%<div id="password-recovery">\s*<div class="alert alert-info">\s*<p>(.*?)</p>\s*<a href="/cgi-bin/koha/opac-main.pl">Return to the main page</a>\s*</div>%s', $postResults, $messageInformation) ||
 					preg_match('%<div id="password-recovery">\s+<div class="alert alert-info">(.*?)<a href="/cgi-bin/koha/opac-main.pl">Return to the main page</a>\s+</div>\s+</div>%s', $postResults, $messageInformation)) {
 				$message = $messageInformation[1];
 				$result['success'] = true;
+				$result['foundPatron'] = true;
 				$result['message'] = translate([
 					'text' => trim($message),
 					'isPublicFacing' => true,
