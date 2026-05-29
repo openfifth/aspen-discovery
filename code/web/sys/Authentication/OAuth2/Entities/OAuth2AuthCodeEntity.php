9c91fe0c5826c492476ddb26f10a4078d3173536
kirstien <kirstienkroeger@icloud.com>
2026-05-29 17:38:23 -0500

    DIS-2214: clean up entities

--- code/web/sys/Authentication/OAuth2/Entities/OAuth2AuthCodeEntity.php
+++ code/web/sys/Authentication/OAuth2/Entities/OAuth2AuthCodeEntity.php
@@ -1,16 +1,14 @@
 <?php
 
 use League\OAuth2\Server\Entities\AuthCodeEntityInterface;
-use League\OAuth2\Server\Entities\Traits\AuthCodeTrait;
-use League\OAuth2\Server\Entities\Traits\EntityTrait;
-use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
 
 /**
  * OAuth2 Authorization Code Entity
  * Implements the League OAuth2 Server AuthCodeEntityInterface
  */
 class OAuth2AuthCodeEntity implements AuthCodeEntityInterface {
-	use EntityTrait, TokenEntityTrait, AuthCodeTrait;
 
 	protected ?string $codeChallenge = null;
 	protected ?string $codeChallengeMethod = null;
@@ -41,5 +39,93 @@ class OAuth2AuthCodeEntity implements AuthCodeEntityInterface {
 	 */
 	public function setCodeChallengeMethod(?string $codeChallengeMethod): void {
 		$this->codeChallengeMethod = $codeChallengeMethod;
+	}
+
+	public function getRedirectUri(): string|null {
+		return $this->redirecturi;
+	}
+
+	public function setRedirectUri(string $uri): void {
+		$this->redirecturi = $uri;
+	}
+
+	/**
+	 * Get the token's identifier.
+	 *
+	 * @return non-empty-string
+	 */
+	public function getIdentifier(): string {
+		return $this->identifier;
+	}
+
+	/**
+	 * Set the token's identifier.
+	 *
+	 * @param non-empty-string $identifier
+	 */
+	public function setIdentifier(string $identifier): void {
+		$this->identifier = $identifier;
+	}
+
+	/**
+	 * Get the token's expiry date time.
+	 */
+	public function getExpiryDateTime(): DateTimeImmutable {
+		return $this->expiryDateTime;
+	}
+
+	/**
+	 * Set the date time when the token expires.
+	 */
+	public function setExpiryDateTime(DateTimeImmutable $dateTime): void {
+		$this->expiryDateTime = $dateTime;
+	}
+
+	/**
+	 * Set the identifier of the user associated with the token.
+	 *
+	 * @param non-empty-string $identifier
+	 */
+	public function setUserIdentifier(string $identifier): void {
+		$this->userIdentifier = $identifier;
+	}
+
+	/**
+	 * Get the token user's identifier.
+	 *
+	 * @return non-empty-string|null
+	 */
+	public function getUserIdentifier(): string|null {
+		return $this->userIdentifier;
+	}
+
+	/**
+	 * Get the client that the token was issued to.
+	 */
+	public function getClient(): ClientEntityInterface {
+		return $this->client;
+	}
+
+	/**
+	 * Set the client that the token was issued to.
+	 */
+	public function setClient(ClientEntityInterface $client): void {
+		$this->client = $client;
+	}
+
+	/**
+	 * Associate a scope with the token.
+	 */
+	public function addScope(ScopeEntityInterface $scope): void {
+		$this->scopes[] = $scope;
+	}
+
+	/**
+	 * Return an array of scopes associated with the token.
+	 *
+	 * @return array
+	 */
+	public function getScopes(): array {
+		return $this->scopes;
 	}
 }
--- code/web/sys/Authentication/OAuth2/Entities/OAuth2ClientEntity.php
+++ code/web/sys/Authentication/OAuth2/Entities/OAuth2ClientEntity.php
@@ -1,14 +1,11 @@
 <?php
 
 use League\OAuth2\Server\Entities\ClientEntityInterface;
-use League\OAuth2\Server\Entities\Traits\ClientTrait;
-use League\OAuth2\Server\Entities\Traits\EntityTrait;
 
 /**
  * OAuth2 Client Entity implementation
  */
 class OAuth2ClientEntity implements ClientEntityInterface {
-	use EntityTrait, ClientTrait;
 
 	public function setName($name): void {
 		$this->name = $name;
@@ -20,5 +17,42 @@ class OAuth2ClientEntity implements ClientEntityInterface {
 
 	public function setIsConfidential($isConfidential = true): void {
 		$this->isConfidential = $isConfidential;
+	}
+
+	public function setIdentifier(string $getClientId): void {
+		$this->identifier = $getClientId;
+	}
+
+	/**
+	 * Get the client's identifier.
+	 *
+	 * @return non-empty-string
+	 */
+	public function getIdentifier(): string {
+		return $this->identifier;
+	}
+
+	/**
+	 * Get the client's name.
+	 */
+	public function getName(): string {
+		return $this->name;
+	}
+
+	/**
+	 * Returns the registered redirect URI (as a string). Alternatively return
+	 * an indexed array of redirect URIs.
+	 *
+	 * @return string|string[]
+	 */
+	public function getRedirectUri(): string|array {
+		return $this->redirectUri;
+	}
+
+	/**
+	 * Returns true if the client is confidential.
+	 */
+	public function isConfidential(): bool {
+		return $this->isConfidential;
 	}
 }
