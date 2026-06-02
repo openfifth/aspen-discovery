641dd166e484cc6fa501aa4daa3a961f0a12f5bc
Kodi <kodilein@gmail.com>
2026-06-01 17:49:23 -0700

    DIS-2396: Generate Series Based on Title, Author, Language
    - Add version for series indexing settings
    - Add permanent id and language columns to series table
    - Update property.tpl to have new warning text for series version updates
    - Add series author to seriesWithVolume
    - Add logic for creating a series permanent id
    - Add logic to generate/group series titles based on series title, author, and language
    - Update release notes
    
    Tested on my local instance of Aspen

--- code/web/sys/DBMaintenance/version_updates/26.06.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.06.00.php
@@ -20,6 +20,24 @@ function getUpdates26_06_00(): array {
 		//kirstien
 
 		//kodi
+		'series_columns' => [
+			'title' => 'Add Columns in Series Table',
+			'description' => 'Add columns in series table for permanent id and language',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE series ADD COLUMN seriesPermanentId CHAR(40)',
+				'ALTER TABLE series ADD COLUMN seriesLanguage VARCHAR(20)',
+			]
+		], //series_columns
+		'series_setting_version' => [
+			'title' => 'Add Column in Series Indexing Settings Table',
+			'description' => 'Add column in series settings indexing table for version',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE series_indexing_settings ADD COLUMN version tinyint(1) DEFAULT 0',
+				'ALTER TABLE series_indexing_settings ADD COLUMN truncateForVersionSwitch TINYINT(1) NOT NULL DEFAULT 0',
+			]
+		], //series_setting_version
 
 		//yanjun
 
