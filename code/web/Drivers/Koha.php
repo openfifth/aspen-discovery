257d87fa16f7b0933e199c653ee9dbad2773bd2b
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-07-03 15:33:30 +0100

    DIS-2637: fix: display patron extended attribute values
    (cherry picked from commit 9a59d987362eb6a255bf056add5688fb90dafc1a)
    Signed-off-by: Chloé Zermatten <chloe.zermatten@openfifth.co.uk>

--- code/web/Drivers/Koha.php
+++ code/web/Drivers/Koha.php
@@ -7637,7 +7637,7 @@ class Koha extends AbstractIlsDriver {
 		$response = $this->kohaApiUserAgent->get("/api/v1/patrons/$borrowerNumber/extended_attributes",'koha.getUserExtendedAttributes',[],[]);
 		$responseCode = $response['code'];
 		if ($responseCode == 200) {
-			$body = $response['body'] ?? [];
+			$body = $response['content'] ?? [];
 			foreach($body as $elem ) { 
 				$attribute = [
 					'id' => $elem['extended_attribute_id'],
