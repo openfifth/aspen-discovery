8172fa5e722b47fe87e6a43de20d795714624ed9
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-07-08 13:05:35 +0100

    Place LiDA holds as the linked patron passed via userId

--- code/web/services/API/UserAPI.php
+++ code/web/services/API/UserAPI.php
@@ -1985,13 +1985,22 @@ class UserAPI extends AbstractAPI {
 			global $library;
 			if ($library->showHoldButton) {
 				if ($source == 'ils' || $source == null) {
+					$patronId = $_REQUEST['userId'] ?? $user->id;
+					$patron = $user->getUserReferredTo($patronId);
+					if (!$patron) {
+						return [
+							'success' => false,
+							'title' => 'Error',
+							'message' => 'Sorry, it looks like you don\'t have access to that patron.',
+						];
+					}
 					if (isset($_REQUEST['pickupBranch']) || isset($_REQUEST['campus'])) {
 						if (isset($_REQUEST['pickupBranch'])) {
 							if (empty($_REQUEST['pickupBranch'])) {
 								$location = new Location();
-								$userPickupLocations = $location->getPickupBranches($user);
+								$userPickupLocations = $location->getPickupBranches($patron);
 								foreach ($userPickupLocations as $tmpLocation) {
-									if ($tmpLocation->code == $user->getPickupLocationCode()) {
+									if ($tmpLocation->code == $patron->getPickupLocationCode()) {
 										$pickupBranch = $tmpLocation->code;
 										break;
 									}
@@ -2002,7 +2011,7 @@ class UserAPI extends AbstractAPI {
 						} else {
 							$pickupBranch = trim($_REQUEST['campus']);
 						}
-						$locationValid = $user->validatePickupBranch($pickupBranch);
+						$locationValid = $patron->validatePickupBranch($pickupBranch);
 						if (!$locationValid) {
 							return [
 								'success' => false,
@@ -2013,20 +2022,20 @@ class UserAPI extends AbstractAPI {
 							];
 						}
 					} else {
-						$pickupBranch = $user->_homeLocationCode;
+						$pickupBranch = $patron->_homeLocationCode;
 					}
 
 					if (isset($_REQUEST['rememberHoldPickupLocation']) && $library->allowRememberPickupLocation) {
-						$user->setRememberHoldPickupLocation($_REQUEST['rememberHoldPickupLocation']);
+						$patron->setRememberHoldPickupLocation($_REQUEST['rememberHoldPickupLocation']);
 					}
 
-					if ($library->allowPickupLocationUpdates && $user->rememberHoldPickupLocation) {
+					if ($library->allowPickupLocationUpdates && $patron->rememberHoldPickupLocation) {
 						if (isset($_REQUEST['pickupBranch'])) {
 							$pickupLocation = new Location();
 							$pickupLocation->code = $_REQUEST['pickupBranch'];
 							if ($pickupLocation->find(true)) {
-								if ($pickupLocation->locationId != $user->pickupLocationId) {
-									$user->setPickupLocationId($pickupLocation->locationId);
+								if ($pickupLocation->locationId != $patron->pickupLocationId) {
+									$patron->setPickupLocationId($pickupLocation->locationId);
 								}
 							}
 
@@ -2036,18 +2045,18 @@ class UserAPI extends AbstractAPI {
 								$sublocation->id = $_REQUEST['pickupSublocation'];
 								if ($sublocation->find(true)) {
 									if ($pickupLocation->locationId == $sublocation->locationId) {
-										if ($sublocation->id != $user->pickupSublocationId) {
-											$user->setPickupSublocationId($sublocation->id);
+										if ($sublocation->id != $patron->pickupSublocationId) {
+											$patron->setPickupSublocationId($sublocation->id);
 										}
 									}
 								}
 							}
 						}
 
-						$user->update();
+						$patron->update();
 					}
 
-					$homeLibrary = $user->getHomeLibrary();
+					$homeLibrary = $patron->getHomeLibrary();
 
 					if (!empty($_REQUEST['cancelDate'])) {
 						$cancelDate = $_REQUEST['cancelDate'];
@@ -2078,7 +2087,7 @@ class UserAPI extends AbstractAPI {
 
 					$holdType = $_REQUEST['holdType'];
 					if ($holdType == 'item' && isset($_REQUEST['itemId'])) {
-						$result = $user->placeItemHold($shortId, $_REQUEST['itemId'], $pickupBranch, $cancelDate, $pickupSublocation);
+						$result = $patron->placeItemHold($shortId, $_REQUEST['itemId'], $pickupBranch, $cancelDate, $pickupSublocation);
 						$action = $result['api']['action'] ?? null;
 						$responseMessage = strip_tags($result['api']['message']);
 						$responseMessage = trim($responseMessage);
@@ -2094,7 +2103,7 @@ class UserAPI extends AbstractAPI {
 							'needsIllRequest' => $needsIllRequest
 						];
 					} elseif ($holdType == 'volume' && isset($_REQUEST['volumeId'])) {
-						$result = $user->placeVolumeHold($shortId, $_REQUEST['volumeId'], $pickupBranch, $pickupSublocation);
+						$result = $patron->placeVolumeHold($shortId, $_REQUEST['volumeId'], $pickupBranch, $pickupSublocation);
 						$action = $result['api']['action'] ?? null;
 						$responseMessage = strip_tags($result['api']['message']);
 						$responseMessage = trim($responseMessage);
@@ -2124,7 +2133,7 @@ class UserAPI extends AbstractAPI {
 								];
 							}
 						}
-						$result = $user->placeHold($bibId, $pickupBranch, $cancelDate, $pickupSublocation);
+						$result = $patron->placeHold($bibId, $pickupBranch, $cancelDate, $pickupSublocation);
 						$action = $result['api']['action'] ?? null;
 						$responseMessage = strip_tags($result['api']['message']);
 						$responseMessage = trim($responseMessage);
