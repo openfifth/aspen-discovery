ee3a12313e488144fdd5f8608c3828447d221f65
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-08-25 14:28:40 +0100

    DIS-164: chore: database update

--- code/web/sys/DBMaintenance/version_updates/26.08.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.08.00.php
@@ -153,10 +153,11 @@ function getUpdates26_08_00(): array {
 		], //add_minimal_self_registration_modal_setting
 		'add_booking_toggles_to_library' => [
 			'title' => 'Add Bookings Toggles to Library',
-			'description' => 'Adds enableBookings, enableBookingUpdates, and enableBookingCancellations flag to the library table to allow per-library-system control of the patron actions in the Koha Bookings integration.',
+			'description' => 'Adds enableBookingDisplay, enableBookingPlacement, enableBookingUpdates, and enableBookingCancellations flags to the library table to allow per-library-system control of the patron actions in the Koha Bookings integration.',
 			'continueOnError' => false,
 			'sql' => [
-				'ALTER TABLE library ADD COLUMN IF NOT EXISTS enableBookings tinyint(1) DEFAULT 0',
+				'ALTER TABLE library ADD COLUMN IF NOT EXISTS enableBookingDisplay tinyint(1) DEFAULT 0',
+				'ALTER TABLE library ADD COLUMN IF NOT EXISTS enableBookingPlacement tinyint(1) DEFAULT 0',
 				'ALTER TABLE library ADD COLUMN IF NOT EXISTS enableBookingUpdates tinyint(1) DEFAULT 0',
 				'ALTER TABLE library ADD COLUMN IF NOT EXISTS enableBookingCancellations tinyint(1) DEFAULT 0',
 			]
@@ -184,7 +185,6 @@ function getUpdates26_08_00(): array {
 					ils_end_date date NOT NULL,
 					ils_pickup_library_id varchar(50) DEFAULT NULL,
 					ils_status varchar(50) DEFAULT NULL,
-					ils_notes text DEFAULT NULL,
 					createdAt int(11) NOT NULL,
 					PRIMARY KEY (id),
 					UNIQUE KEY userId (userId, ils_booking_id)
